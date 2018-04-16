f9e4457f5dc21fb514a97f96d89116c9d4f26c78
Christopher Möller <chrtze@gmail.com>
2018-04-16 18:08:44 +0200

    FMB-50 add netlify integration

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -5,6 +5,6 @@ export default () => (
   <div className="home">
     <Link to="/map"><button>guck dir die Karte an</button></Link>
     <Link to="/fix"><button>melde einen Fix</button></Link>
-    <Link to="/about">Worum geht es hier genau?</Link>
+    <Link to="/about">Worum geht es hier genau??</Link>
   </div>
 );
