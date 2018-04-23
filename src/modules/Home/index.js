aa8c5267053f5cf40f5576a9ffead0c7b6e95164
Christopher Möller <chrtze@gmail.com>
2018-04-23 14:06:21 +0200

    FMB-59 implement and style start screen

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FixMyLogo from '~/images/logofmb@2x.png';
 
 import './Home.styl';
 
@@ -7,16 +8,19 @@ export default () => (
   <div className="home app__content--overlay">
     <div className="home__content">
       <div className="logo__container">
-        <img src="http://via.placeholder.com/80x100" alt="logo" />
+        <img width="70" src={FixMyLogo} alt="logo" />
       </div>
       <div className="intro__container">
         <h1>Hi, das ist FixMyBerlin</h1>
-        <p className="text">Wir wollen, dass Berlin eine richtig gute Fahrradstadt wird. Hier siehst du, wie das gehen kann.</p>
+        <p className="text">Wir wollen, dass Berlin eine richtig gute Fahrradstadt wird. Hier siehst du bald, was dafür geplant wird. Trage dich für Updates zum Newsletter ein.
+        </p>
       </div>
-      <div className="button__container">
-        <Link className="btn btn--secondary" to="/map">guck dir die Karte an</Link>
-        <Link className="btn btn--primary" to="/fix">melde einen Fix</Link>
-        <Link className="btn" to="/info">Worum geht es hier genau?</Link>
+      <form className="intro__newsletter">
+        <input className="input" type="text" placeholder="Deine Emailadresse" />
+        <button className="btn btn--primary newsletter__subscribe" type="submit">Newsletter abonnieren</button>
+      </form>
+      <div className="intro__about">
+        <Link to="/info">Worum geht es hier genau?</Link>
       </div>
     </div>
   </div>
