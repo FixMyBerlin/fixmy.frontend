0a72e4c5833329a6e2764220052ec76f56cb4ddb
moklick <info@moritzklack.com>
2018-08-27 16:42:35 +0200

    FMB-111: add basic 404 page

---
+++ src/modules/NoMatch/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ContentPageWrapper from '~/components/ContentPageWrapper';
+import Title from '~/components/styled/Title';
+import Text from '~/components/styled/Text';
+
+export default () => (
+  <ContentPageWrapper>
+    <Title>404</Title>
+    <Text>Seite nicht gefunden</Text>
+  </ContentPageWrapper>
+);
