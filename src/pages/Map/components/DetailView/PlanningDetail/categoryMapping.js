7d3b06eda009551ba2dbc114d4ceca1a7cdc41a5
moklick <info@moritzklack.com>
2018-09-14 16:27:44 +0200

    FMB-231: add mapping data

---
+++ src/pages/Map/components/DetailView/PlanningDetail/categoryMapping.js
@@ -0,0 +1,12 @@
+export default {
+  'new cycling infrastructure': 'Errichtung Radverkehrsanlage',
+  'renovation of cycling infrastructure': 'Sanierung / Umbau Radweg',
+  'bike street': 'Anordnung Fahrradstraße',
+  'modification of junction': 'Umbau Knotenpunkt',
+  'bike parking': 'Maßn. Fahrradparken',
+  'crossing aid': 'Querungshilfe',
+  'shared space': 'Errichtung Begegnungszone',
+  'modification of cross section': 'Sanierung / Umbau Straßenquerschnitt',
+  miscellaneous: 'Sonstiges',
+  'new street': 'Straßenneubau'
+};
