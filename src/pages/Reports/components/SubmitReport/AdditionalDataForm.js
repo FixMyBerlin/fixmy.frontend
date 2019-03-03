df930c9a7dd7652a75d30c2b7b8193fa5b8c54d1
geomC <sascha.picard@gmail.com>
2019-03-03 20:09:47 +0100

    Start work on AdditionalDataForm

--- src/pages/Reports/components/SubmitReport/AdditionalDataForm.js
+++ src/pages/Reports/components/SubmitReport/AdditionalDataForm.js
@@ -2,20 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const MyInnerComponent = styled.div`
-  
+const Wrapper = styled.div`
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const MyComponent = ({myProp}) => (
-  <MyInnerComponent>{myProp}</MyInnerComponent>
+const Heading = styled.h3`
+  font-size: 22px;
+  font-weight: bold;
+  color: ${config.colors.black};
+`;
+
+const Hint = styled.p`
+  font-size: 14px;
+  color: ${config.colors.darkgrey};
+  line-height: 1.4;
+`;
+
+// TODO: make this a component having subcomponents
+// TODO: checkout https://github.com/odysseyscience/react-s3-uploader for upload component
+
+const AdditionalDataForm = ({photoUploadUrl}) => (
+  <Wrapper>
+    <Heading>Hier kannst du noch ein Foto ergänzen</Heading>
+    <Hint>Das hilft den Planer*innen die Situation vor Ort besser zu verstehen.</Hint>
+
+
+    <Heading>…oder eine Beschreibung eingeben</Heading>
+  </Wrapper>
 );
 
-MyComponent.propTypes = {
-  myProp: PropTypes.string
+AdditionalDataForm.propTypes = {
+  photoUploadUrl: PropTypes.string
 };
 
-MyComponent.defaultProps = {
-  myProp: 'so'
-}
+AdditionalDataForm.defaultProps = {
+  photoUploadUrl: 'please-set-a-url'
+};
 
-export default MyComponent;
+export default AdditionalDataForm;
