c90b4668c5520fa9b25d86dd13a371aaaebff3d7
geomC <sascha.picard@gmail.com>
2019-03-15 21:02:42 +0100

    Add Faq texts

--- src/pages/Reports/components/Landing/FaqSection.js
+++ src/pages/Reports/components/Landing/FaqSection.js
@@ -19,18 +19,22 @@ class FaqSection extends Component {
         },
         {
           heading: 'Wie kann ich mitmachen?',
+          text: 'Klicke unten auf den Button „Jetzt Mitmachen“ dann kommst du zur Karte mit allen bisherigen Meldungen. Klicke dort auf das Plus-Zeichen um eine neue Meldung zu erstellen. Anschliessend wirst du einen kurzen Dialog geführt. Am einfachsten kannst du eine Meldung unterwegs von deinem Smartphone machen, du kannst aber auch von zuhause am PC einen Eintrag erstellen. Für die Meldung brauchst du nichts weiter als eine Emailadresse.',
           collapsed: true // initially collapse all items but the first one
         },
         {
           heading: 'Was genau passiert mit den Meldungen?',
+          text: 'Nach Abschluss des Meldezeitraums werden die Mitarbeiter und Mitarbeiterinnen des Tiefbauamtes in Friedrichshain-Kreuzberg alle Meldungen anschauen und auswerten, welche umsetzbar sind. Du bekommst dann auf jeden Fall eine Rückmeldung, ob dein Wunsch realisiert werden kann. Wenn es Rückfragen gibt, wird sich das Bezirksamt ebenfalls bei dir melden. Zusätzlich wird auf FixMyBerlin veröffentlicht, welche Standorte bald neue Fahrradbügel bekommen.',
           collapsed: true // initially collapse all items but the first one
         },
         {
           heading: 'Werden alle Meldungen umgesetzt?',
+          text: 'Nach Abschluss des Meldezeitraums werden die Mitarbeiter und Mitarbeiterinnen des Tiefbauamtes in Friedrichshain-Kreuzberg alle Meldungen anschauen und auswerten, welche umsetzbar sind. Du bekommst dann auf jeden Fall eine Rückmeldung, ob dein Wunsch realisiert werden kann. Wenn es Rückfragen gibt, wird sich das Bezirksamt ebenfalls bei dir melden. Zusätzlich wird auf FixMyBerlin veröffentlicht, welche Standorte bald neue Fahrradbügel bekommen.',
           collapsed: true // initially collapse all items but the first one
         },
         {
           heading: 'Warum ist mein Bezirk nicht dabei?',
+          text: 'Nach Abschluss des Meldezeitraums werden die Mitarbeiter und Mitarbeiterinnen des Tiefbauamtes in Friedrichshain-Kreuzberg alle Meldungen anschauen und auswerten, welche umsetzbar sind. Du bekommst dann auf jeden Fall eine Rückmeldung, ob dein Wunsch realisiert werden kann. Wenn es Rückfragen gibt, wird sich das Bezirksamt ebenfalls bei dir melden. Zusätzlich wird auf FixMyBerlin veröffentlicht, welche Standorte bald neue Fahrradbügel bekommen.',
           collapsed: true // initially collapse all items but the first one
         }
       ]
@@ -58,7 +62,7 @@ class FaqSection extends Component {
           <FaqItem
             heading={heading}
             text={text}
-            collapsed={ collapsed }
+            collapsed={collapsed}
             onTab={() => this.toggleCollapsed(index)}
             key={heading.replace(/ /g, '')} // not sure what to use here besides the array index
           />
