b26cd13d8d3688029128a3c257a75808e6d5f33e
geomC <sascha.picard@gmail.com>
2019-03-16 19:59:04 +0100

    Landing Page: Use react-collapsible instead of custom solution

--- src/pages/Reports/components/Landing/FaqItem.js
+++ src/pages/Reports/components/Landing/FaqItem.js
@@ -1,116 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
+import Collapsible from 'react-collapsible';
 import PropTypes from 'prop-types';
 
-// TODO: use https://www.npmjs.com/package/react-collapsible
-// TODO: Refactor (Split up in single components#
-// TODO: Follow convenventions on how styled components are used elsewhere)
-// TODO: check of styled-components can do autoprefixing to un-bloat styles
+// TODO: find a good solution to re-append the <hr /> to opened collapsibles. must be done animated to look good
 
-const FaqItemWrapper = styled.div`
+// helper to customize collapsible,
+// following https://github.com/glennflanagan/react-collapsible/blob/develop/example/_src/sass/components/_Collapsible.scss
+const CollapsibleWrapper = styled.div`
 
-  padding: 0 23px 8px 8px;
-
-  &:hover {
-    cursor: pointer;
-  }
-  
-  h3 {
-    line-height: 1.33;
-    margin-bottom: 13px;
-  }
-  
-  hr {
-    margin: 0;
-  }
-   
-  p {
-    padding: 8px 0 24px 0;
-    margin: 0;
-     -webkit-transform: scaleY(1);
-    -ms-transform: scaleY(1);
-    -o-transform: scaleY(1); 
-    transform: scaleY(1);
-    -webkit-transform-origin: top;
-    -ms-transform-origin: top;
-    -o-transform-origin: top; 
-    transform-origin: top;
-    transition: transform 0.26s ease; 
-    max-height: 1000px;
-  }
-
-  h3 i.triangle {
-    float: right;
-    width: 0;
-    height: 0;
-    display: inline-block;
-    border-style: solid;
-    border-width: 12px 7.5px 0 7.5px; // triangle tip on the left
-    border-color: #d8d8d8 transparent transparent transparent;
-    margin-top: 6px;
-    -webkit-transform: rotate(0);
-    -ms-transform: rotate(0);
-    -o-transform: rotate(0); 
-    transform: rotate(0);
-    transition: transform 200ms cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
-  
-  // collapsed state 
-  &.collapsed {
-  
-    h3 i.triangle { 
-      -webkit-transform: rotate(90deg);
-      -ms-transform: rotate(90deg);
-      -o-transform: rotate(90deg); 
-      transform: rotate(90deg); 
-      transition: transform 200ms cubic-bezier(0.215, 0.61, 0.355, 1);
-    }
-    
+  .Collapsible__contentInner {
     p {
-      -webkit-transform:  scaleY(0);
-      -ms-transform:  scaleY(0);
-      -o-transform:  scaleY(0); 
-      transform: scaleY(0);
-      -webkit-transform-origin: top;
-      -ms-transform-origin: top;
-      -o-transform-origin: top; 
-      transform-origin: top;
-      transition: all 0.26s ease; 
-      max-height: 2em;
-      //transition:  max-height 200ms cubic-bezier(0.215, 0.61, 0.355, 1) 50ms;
+       font-size: 16px;
+       color: ${config.colors.darkgrey};
     }
- }
-`;
-
-const StyledHr = styled.hr`
-  width: 100%;
-  border: 0.5px solid ${config.colors.inactivegrey};
-`;
-
-const FaqItem = ({ heading, text, collapsed, onTab }) => (
-  <FaqItemWrapper onClick={onTab} className={collapsed && 'collapsed'}>
-    <h3>{heading}
-      {
-        // only show arrow if there is text to show/hide
-        text && <i className="triangle" />
+  }
+  
+  //The link which when clicked opens the collapsable area
+  .Collapsible__trigger {
+    display: block;
+    font-size: 16px;
+    font-weight: bold;
+    position: relative;
+    line-height: 2.5;
+    color: ${config.colors.black};
+ 
+    &:after {
+      font-family: 'Open Sans', sans-serif;
+      content: '';
+      border-style: solid;
+      border-width: 12px 7.5px 0 7.5px; // triangle
+      transform: rotateZ(90deg);
+      border-color: #d8d8d8 transparent transparent transparent;
+      position: absolute;
+      right: 0;
+      top: 15px;
+      display: block;
+      transition: transform 300ms;
+    }
+  
+    &.is-open {
+      &:after {
+        transform: rotateZ(0);
       }
-    </h3>
-    <p>{text}</p>
-    {!collapsed && (<StyledHr />)}
-  </FaqItemWrapper>
+    }
+  
+    &.is-disabled {
+      opacity: 0.5;
+      background-color: grey;
+    }
+  }
+`;
+
+
+const FaqItem = ({ heading, text, open }) => (
+  <CollapsibleWrapper>
+    <Collapsible trigger={heading} open={open}>
+      <p>{text}</p>
+    </Collapsible>
+  </CollapsibleWrapper>
 );
 
 
 FaqItem.propTypes = {
   heading: PropTypes.string,
   text: PropTypes.string,
-  collapsed: PropTypes.bool
+  open: PropTypes.bool
 };
 
 FaqItem.defaultProps = {
   heading: 'Dieser Abschnitt hat noch keine Überschrift',
   text: 'Der Text zu diesem Abschnitt in Arbeit...',
-  collapsed: false
+  open: false
 };
 
 export default FaqItem;
--- src/pages/Reports/components/Landing/FaqSection.js
+++ src/pages/Reports/components/Landing/FaqSection.js
@@ -3,67 +3,51 @@ import styled from 'styled-components';
 import FaqItem from './FaqItem';
 
 const FaqSectionWrapper = styled.div`
-  margin-bottom: 72px;
+  margin-bottom: 62px;
+  padding: 8px;
 `;
 
 class FaqSection extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // TODO: propably better make this configurable
+      // will eventually be ajaxed later
       content: [
         {
           heading: 'Warum solltest du mitmachen?',
           text: 'Im Rahmen des Mobilitätsgesetzes sollen in ganz Berlin 100.000 neue Abstellplätze für den Radverkehr geschaffen werden. Friedrichshain-Kreuzberg setzt sich für sicheres Fahrradparken im Bezirk ein und möchte möglichst viele der Meldungen umsetzen. \n' +
-          'Ihr könnt mit eurer Bedarfsmeldung aktiv an einer Verbesserung der Rad-Infrastruktur des Bezirks mithelfen indem ihr beschreibt wo neue Fahrradbügel benötigt werden.'
+          'Ihr könnt mit eurer Bedarfsmeldung aktiv an einer Verbesserung der Rad-Infrastruktur des Bezirks mithelfen indem ihr beschreibt wo neue Fahrradbügel benötigt werden.',
+          open: true // initially uncollapsed
         },
         {
           heading: 'Wie kann ich mitmachen?',
           text: 'Klicke unten auf den Button „Jetzt Mitmachen“ dann kommst du zur Karte mit allen bisherigen Meldungen. Klicke dort auf das Plus-Zeichen um eine neue Meldung zu erstellen. Anschliessend wirst du einen kurzen Dialog geführt. Am einfachsten kannst du eine Meldung unterwegs von deinem Smartphone machen, du kannst aber auch von zuhause am PC einen Eintrag erstellen. Für die Meldung brauchst du nichts weiter als eine Emailadresse.',
-          collapsed: true // initially collapse all items but the first one
         },
         {
           heading: 'Was genau passiert mit den Meldungen?',
           text: 'Nach Abschluss des Meldezeitraums werden die Mitarbeiter und Mitarbeiterinnen des Tiefbauamtes in Friedrichshain-Kreuzberg alle Meldungen anschauen und auswerten, welche umsetzbar sind. Du bekommst dann auf jeden Fall eine Rückmeldung, ob dein Wunsch realisiert werden kann. Wenn es Rückfragen gibt, wird sich das Bezirksamt ebenfalls bei dir melden. Zusätzlich wird auf FixMyBerlin veröffentlicht, welche Standorte bald neue Fahrradbügel bekommen.',
-          collapsed: true // initially collapse all items but the first one
         },
         {
           heading: 'Werden alle Meldungen umgesetzt?',
           text: 'Nach Abschluss des Meldezeitraums werden die Mitarbeiter und Mitarbeiterinnen des Tiefbauamtes in Friedrichshain-Kreuzberg alle Meldungen anschauen und auswerten, welche umsetzbar sind. Du bekommst dann auf jeden Fall eine Rückmeldung, ob dein Wunsch realisiert werden kann. Wenn es Rückfragen gibt, wird sich das Bezirksamt ebenfalls bei dir melden. Zusätzlich wird auf FixMyBerlin veröffentlicht, welche Standorte bald neue Fahrradbügel bekommen.',
-          collapsed: true // initially collapse all items but the first one
         },
         {
           heading: 'Warum ist mein Bezirk nicht dabei?',
           text: 'Nach Abschluss des Meldezeitraums werden die Mitarbeiter und Mitarbeiterinnen des Tiefbauamtes in Friedrichshain-Kreuzberg alle Meldungen anschauen und auswerten, welche umsetzbar sind. Du bekommst dann auf jeden Fall eine Rückmeldung, ob dein Wunsch realisiert werden kann. Wenn es Rückfragen gibt, wird sich das Bezirksamt ebenfalls bei dir melden. Zusätzlich wird auf FixMyBerlin veröffentlicht, welche Standorte bald neue Fahrradbügel bekommen.',
-          collapsed: true // initially collapse all items but the first one
         }
       ]
     };
   }
 
-
-  toggleCollapsed = (indexOfToggledItem) => {
-    this.setState(prevState => ({
-      content: prevState.content.map((item, index) => {
-        const newItem = item;
-        if (index === indexOfToggledItem) {
-          newItem.collapsed = !newItem.collapsed;
-        }
-        return newItem;
-      })
-    }));
-  };
-
   render() {
     const faqItems = this.state.content
-      .map((item, index) => {
-        const { heading, text, collapsed } = item;
+      .map((item) => {
+        const { heading, text, open } = item;
         return (
           <FaqItem
             heading={heading}
             text={text}
-            collapsed={collapsed}
-            onTab={() => this.toggleCollapsed(index)}
+            open={open}
             key={heading.replace(/ /g, '')} // not sure what to use here besides the array index
           />
         );
