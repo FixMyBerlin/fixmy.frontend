ce9a06fc6c6f78a5c2bfd37fc85c439fd0d79d8a
geomC <sascha.picard@gmail.com>
2019-04-04 10:37:07 +0200

    Commit assets not yet checked in

---
+++ src/pages/Reports/components/ReportMarkers.js
@@ -0,0 +1,87 @@
+import { PureComponent } from 'react';
+import MapboxGL from 'mapbox-gl';
+
+import DraftMarker from '~/images/planning-icons/konzept-marker.png';
+import PlanningMarker from '~/images/planning-icons/planung-marker.png';
+import ExecutionMarker from '~/images/planning-icons/bau-marker.png';
+import ReadyMarker from '~/images/planning-icons/fertig-marker.png';
+
+import { getCenterFromGeom } from '~/pages/Map/map-utils';
+
+const Markers = {
+  draft: DraftMarker,
+  planning: PlanningMarker,
+  execution: ExecutionMarker,
+  ready: ReadyMarker
+};
+
+const phasesOrder = Object.keys(Markers);
+
+class PlanningMarkers extends PureComponent {
+  constructor() {
+    super();
+
+    this.markers = [];
+  }
+
+  componentDidMount() {
+    this.updateMarkers();
+  }
+
+  componentDidUpdate() {
+    this.updateMarkers();
+  }
+
+  removeMarkers = () => {
+    this.markers.forEach((marker) => {
+      if (marker) {
+        marker.remove();
+      }
+    });
+    this.markers = [];
+  }
+
+  updateMarkers = () => {
+    const { active, data, map } = this.props;
+    if (!data || !map) {
+      return false;
+    }
+
+    this.removeMarkers();
+
+    if (!active) {
+      return false;
+    }
+
+    this.markers = data.map((d) => {
+      if (!Markers[d.phase]) {
+        return null;
+      }
+
+      const phaseIndex = phasesOrder.indexOf(d.phase);
+      if (!this.props.filterPlannings[phaseIndex]) {
+        return null;
+      }
+
+      const center = getCenterFromGeom(d.geometry);
+      const el = document.createElement('div');
+      el.className = 'marker';
+      el.innerHTML = `<img class="marker-image" src="${Markers[d.phase]}" />`;
+      el.dataset.phase = d.phase;
+      el.addEventListener('click', evt => this.props.onClick(evt, d));
+
+      return new MapboxGL.Marker(el)
+        .setLngLat(center)
+        .setOffset([0, -20])
+        .addTo(map);
+    });
+
+    return true;
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default PlanningMarkers;
