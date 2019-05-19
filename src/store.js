dc7e8a2d87a29ff784d7bf6723068af6f83d9f07
geomC <sascha.picard@gmail.com>
2019-05-19 19:52:23 +0200

    Revert Merge done accidently This reverts commit 88a86345da37edc38de40d14e08fcccc6b91df8e, reversing changes made to 990b551be249e057a07129758e14410a38eed173.

--- src/store.js
+++ src/store.js
@@ -1,14 +1,11 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 import AppState from '~/AppState';
 import MapState from '~/pages/Map/MapState';
 import UserState from '~/pages/User/UserState';
 import AnalysisState from '~/pages/Analysis/AnalysisState';
 import ReportsState from '~/pages/Reports/ReportsState';
-
 
 const Reducer = combineReducers({
   AppState,
@@ -18,36 +15,12 @@ const Reducer = combineReducers({
   ReportsState
 });
 
-const loggerMiddleware = createLogger({
-    diff: true
-  });
+const enhancers = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
-function configureStoreProd(initialState) {
-  const middlewares = [
-    // add middlewares you want in productuon
-  ];
-
-  return createStore(Reducer, initialState, compose(
-    applyMiddleware(...middlewares)
-    ));
-}
-
-function configureStoreDev(initialState) {
-  const middlewares = [
-    thunk,
-    // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
-    reduxImmutableStateInvariant()
-  ];
-  if (config.useReduxLogger) middlewares.push(loggerMiddleware);
-
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  const store = createStore(Reducer, initialState, composeEnhancers(
-    applyMiddleware(...middlewares)
-    ));
-
-  return store;
-}
-
-const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
-
-export default configureStore();
+export default createStore(
+  Reducer,
+  enhancers
+);
