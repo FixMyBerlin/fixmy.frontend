57bbbb66c824d638374667b934bb7880424dea96
geomC <sascha.picard@gmail.com>
2019-07-04 21:18:25 +0200

    improve(AutocompleteGeocoder): cancel running requests on new searchterm enter
    regards #34

--- src/components/AutocompleteGeocoder/api.js
+++ src/components/AutocompleteGeocoder/api.js
@@ -1,15 +1,33 @@
-import ky from 'ky';
+/* eslint-disable no-use-before-define */
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 
-const compileSearchUrl = (searchString) => {
+let abortController = new window.AbortController();
+
+const fetchSuggestions = async (searchString) => {
+  abortController.abort(); // Cancel the previous request
+  abortController = new AbortController();
+  const { signal } = abortController;
+
+  const url = compileSearchUrl(searchString);
+  return fetch(url, { signal })
+    .then(res => res.json())
+    .catch((error) => {
+      // if (error.name === 'AbortError') { FIXME: documented way of detecting an abortError won't work
+      if (error.message.includes('aborted')) { // workaround
+        console.log('cancelled');
+        return { suggestions: [] };
+      }
+    });
+};
+
+function compileSearchUrl(searchString) {
   const { suggestionGeocoderUrl, geocoderAppId, geocoderAppCode } = config.map;
   return `${suggestionGeocoderUrl}?&` +
     `app_id=${geocoderAppId}&app_code=${geocoderAppCode}&` +
     `query=${searchString}&` +
     'beginHighlight=<>&endHighlight=<>&' +
     `mapview=${config.map.geocoderBounds}`;
-};
-
-const fetchSuggestions = searchString => ky.get(compileSearchUrl(searchString)).json();
+}
 
 export default {
   fetchSuggestions
