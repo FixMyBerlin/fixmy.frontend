60e7573205bff49103f952a34dcb90a67122d82c
geomC <sascha.picard@gmail.com>
2019-09-18 20:53:43 +0200

    refactor: remove dead code

--- src/pages/Reports/pages/OverviewMap/state/reducers.js
+++
@@ -1,5 +0,0 @@
-const initalState = {
-    reports: [], // report items fetched from api
-    selectedReport: null, // an entry within reports
-    selectedReportPosition: { x: 0, y: 0 }, // projected position of report popup
-}
