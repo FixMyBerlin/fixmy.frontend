4a9a1c213cd8b106c9c2787c5af93dbde89c5e2c
geomC <sascha.picard@gmail.com>
2019-09-21 20:07:00 +0200

    doc

--- src/pages/Reports/state/OverviewMapState.js
+++ src/pages/Reports/state/OverviewMapState.js
@@ -57,7 +57,7 @@ async function loadReportsThunk(dispatch) {
 
 const initialState = {
   reports: [], // report items fetched from api
-  selectedReport: null, // an entry within reports
+  selectedReport: null, // an item of the reports list
   selectedReportPosition: { x: 0, y: 0 } // projected position of report popup
 };
 
