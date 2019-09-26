d824d9c42fa57aebd2e2dd2c4d3b5e9a6489c171
Vincent Ahrend <mail@vincentahrend.com>
2019-09-26 12:49:57 +0200

    Remove legacy component needed for share button

--- src/pages/Reports/pages/OverviewMap/components/ReportDetails/Footer.js
+++ src/pages/Reports/pages/OverviewMap/components/ReportDetails/Footer.js
@@ -4,12 +4,6 @@ import styled from 'styled-components';
 import PlanningLike from '~/pages/Map/components/DetailView/PlanningDetail/PlanningLike';
 import DetailFooter from '~/pages/Map/components/DetailView/DetailFooter';
 
-// using an invisible item to align the LikeButton in the middle and the share button right using justify-content: space-between;
-const Fill = styled.div`
-  width: 20%;
-  height: 20%;
-`;
-
 const Footer = styled(DetailFooter)`
   width: 100%;
   position: absolute;
@@ -18,14 +12,12 @@ const Footer = styled(DetailFooter)`
 
 const DetailsFooter = ({ token, reportId }) => (
   <Footer>
-    <Fill />
     <PlanningLike
       token={token}
       url={`${config.apiUrl}/reports/${reportId}`}
       id={reportId}
       itemType="Meldung"
     />
-    <Fill />
   </Footer>
 );
 
