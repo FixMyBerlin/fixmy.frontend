e030b9c5201c2babace0c8bda82687dc689a9ba3
Vincent Ahrend <mail@vincentahrend.com>
2019-11-06 10:59:31 +0100

    Update schema definitions

--- src/pages/KatasterKI/scheme/generate.sh
+++ src/pages/KatasterKI/scheme/generate.sh
@@ -1,1 +1,7 @@
-npx typescript-json-schema src/pages/KatasterKI/state.ts Submission --ignoreErrors -o src/pages/KatasterKI/scheme/submission.schema.json
+npx typescript-json-schema src/pages/KatasterKI/types.ts ProfileRequest --ignoreErrors -o src/pages/KatasterKI/scheme/profile-request.schema.json
+npx typescript-json-schema src/pages/KatasterKI/types.ts ProfileResponse --ignoreErrors -o src/pages/KatasterKI/scheme/profile-response.schema.json
+
+npx typescript-json-schema src/pages/KatasterKI/types.ts AnswerRequest --ignoreErrors -o src/pages/KatasterKI/scheme/answer-request.schema.json
+
+npx typescript-json-schema src/pages/KatasterKI/types.ts PerspectiveChangeRequest --ignoreErrors -o src/pages/KatasterKI/scheme/perspective-change-request.schema.json
+npx typescript-json-schema src/pages/KatasterKI/types.ts PerspectiveChangeResponse --ignoreErrors -o src/pages/KatasterKI/scheme/perspective-change-response.schema.json
