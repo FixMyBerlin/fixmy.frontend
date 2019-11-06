41aef1f06822ce7ab63ec60daa24e3a0e7c28b71
Vincent Ahrend <mail@vincentahrend.com>
2019-11-06 14:36:26 +0100

    Add typescript-json-schema dep

--- src/pages/KatasterKI/scheme/generate.sh
+++ src/pages/KatasterKI/scheme/generate.sh
@@ -1,7 +1,7 @@
-npx typescript-json-schema src/pages/KatasterKI/types.ts ProfileRequest --ignoreErrors -o src/pages/KatasterKI/scheme/profile-request.schema.json
-npx typescript-json-schema src/pages/KatasterKI/types.ts ProfileResponse --ignoreErrors -o src/pages/KatasterKI/scheme/profile-response.schema.json
+typescript-json-schema src/pages/KatasterKI/types.ts ProfileRequest --ignoreErrors -o src/pages/KatasterKI/scheme/profile-request.schema.json
+typescript-json-schema src/pages/KatasterKI/types.ts ProfileResponse --ignoreErrors -o src/pages/KatasterKI/scheme/profile-response.schema.json
 
-npx typescript-json-schema src/pages/KatasterKI/types.ts AnswerRequest --ignoreErrors -o src/pages/KatasterKI/scheme/answer-request.schema.json
+typescript-json-schema src/pages/KatasterKI/types.ts AnswerRequest --ignoreErrors -o src/pages/KatasterKI/scheme/answer-request.schema.json
 
-npx typescript-json-schema src/pages/KatasterKI/types.ts PerspectiveChangeRequest --ignoreErrors -o src/pages/KatasterKI/scheme/perspective-change-request.schema.json
-npx typescript-json-schema src/pages/KatasterKI/types.ts PerspectiveChangeResponse --ignoreErrors -o src/pages/KatasterKI/scheme/perspective-change-response.schema.json
+typescript-json-schema src/pages/KatasterKI/types.ts PerspectiveChangeRequest --ignoreErrors -o src/pages/KatasterKI/scheme/perspective-change-request.schema.json
+typescript-json-schema src/pages/KatasterKI/types.ts PerspectiveChangeResponse --ignoreErrors -o src/pages/KatasterKI/scheme/perspective-change-response.schema.json
