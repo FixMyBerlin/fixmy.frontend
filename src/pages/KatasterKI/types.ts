36b559d90054a09494af30f66a16b54bf0c8b8a5
Vincent Ahrend <mail@vincentahrend.com>
2019-11-14 17:40:21 +0100

    Add answer request bindings

--- src/pages/KatasterKI/types.ts
+++ src/pages/KatasterKI/types.ts
@@ -33,10 +33,13 @@ export interface ProfileResponse {
   ratings_total: number;
 }
 
-export interface AnswerRequest extends Answer {}
+export interface AnswerRequest extends Answer {
+  sessionID: string;
+}
 
 export interface PerspectiveRequest {
   perspective: Perspective;
+  sessionID: string;
 }
 
 export interface PerspectiveResponse extends ProfileResponse {}
