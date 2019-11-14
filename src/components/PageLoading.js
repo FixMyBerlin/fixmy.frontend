2a8ca7a07b2abb65d90f3e794c69b91c3b8d0b97
Vincent Ahrend <mail@vincentahrend.com>
2019-11-14 17:34:21 +0100

    Surface error messages in user interface

--- src/components/PageLoading.js
+++ src/components/PageLoading.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Loader = ({ pastDelay, error }) => {
   if (error) {
     if (config.debug) console.error(error);
-    return <p>Ups, da ist etwas schiefgegangen.</p>;
+    return (
+      <p>
+        <span role="img" aria-label="sick face emoji">
+          🤕
+        </span>{' '}
+        Ups, da ist etwas schiefgegangen: {error}
+      </p>
+    );
   }
   if (pastDelay)
     return (
