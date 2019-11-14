a048c47c531597e65a3d75c0a3f4a6e1664ead6e
Vincent Ahrend <mail@vincentahrend.com>
2019-11-14 12:10:43 +0100

    Remove broken integration test

--- src/pages/KatasterKI/tests/state.integration.test.ts
+++ src/pages/KatasterKI/tests/state.integration.test.ts
@@ -5,19 +5,15 @@ import fetchMock from 'fetch-mock';
 import {
   ProfileRequest,
   ProfileResponse,
-  TransportRating,
-  UserGroup,
   Perspective,
   PerspectiveRequest,
-  PerspectiveResponse,
-  TransportMode
+  PerspectiveResponse
 } from '../types';
 import { getEndpointURL } from '../api/utils';
 
 import reducer, {
   State,
   SUBMIT_PROFILE_COMPLETE,
-  SUBMIT_PROFILE_ERROR,
   SUBMIT_PROFILE_PENDING,
   RECEIVED_SCENE_GROUP,
   submitProfile,
@@ -25,18 +21,7 @@ import reducer, {
   SUBMIT_PERSPECTIVE_PENDING,
   SUBMIT_PERSPECTIVE_COMPLETE,
   submitPerspective,
-  testingDefaultState,
-  productionDefaultState,
-  setTOSAccepted,
-  setProfileAnswer,
-  setTransportRating,
-  setZipcode,
-  SET_TOS_ACCEPTED,
-  SET_PROFILE_ANSWER,
-  SET_TRANSPORT_RATING,
-  SET_ZIPCODE,
-  updateProgressBar,
-  UPDATE_PROGRESS_BAR
+  testingDefaultState
 } from '../state';
 
 const profileRequestSample: ProfileRequest = require('../scheme/sample-instances/profile-request-sample-instance.json');
@@ -159,78 +144,5 @@ describe('submitPerspective', () => {
       SUBMIT_PERSPECTIVE_COMPLETE
     ];
     expect(dispatchedActionTypes).toEqual(expectedActions);
-  });
-});
-describe('Profile section', () => {
-  afterEach(() => {
-    fetchMock.restore();
-  });
-
-  it('Submits a profile to the backend after completing all questions', async () => {
-    // mock api request
-    const sessionId = 'session-id';
-    fetchMock.postOnce(
-      getEndpointURL('profile', sessionId, null),
-      profileResponseSample
-    );
-
-    // mock store
-    const stateBefore = {
-      KatasterKIState: {
-        ...productionDefaultState
-      }
-    };
-    const store = mockStore(stateBefore);
-    const expectedActions = [
-      SET_TOS_ACCEPTED,
-      UPDATE_PROGRESS_BAR,
-      SET_PROFILE_ANSWER,
-      SET_TRANSPORT_RATING,
-      SET_PROFILE_ANSWER,
-      SET_PROFILE_ANSWER,
-      SET_PROFILE_ANSWER,
-      SET_PROFILE_ANSWER,
-      SET_PROFILE_ANSWER,
-      SET_PROFILE_ANSWER,
-      SET_PROFILE_ANSWER,
-      SET_PROFILE_ANSWER,
-      SET_ZIPCODE,
-      SET_ZIPCODE,
-      SET_ZIPCODE,
-      SET_ZIPCODE,
-      SET_ZIPCODE,
-      SET_PROFILE_ANSWER,
-      SUBMIT_PROFILE_PENDING,
-      RECEIVED_SCENE_GROUP,
-      SUBMIT_PROFILE_COMPLETE
-    ];
-
-    // Update progress bar omitted
-    store.dispatch(setTOSAccepted(true));
-    store.dispatch(updateProgressBar(12, 13));
-    store.dispatch(setProfileAnswer('berlinTraffic', 3));
-    store.dispatch(setTransportRating(TransportMode.car, 5));
-    store.dispatch(setProfileAnswer('ageGroup', 7));
-    store.dispatch(setProfileAnswer('hasChildren', false));
-    store.dispatch(setProfileAnswer('gender', 'd'));
-    store.dispatch(setProfileAnswer('vehiclesOwned', { car: true }));
-    store.dispatch(setProfileAnswer('bicycleUse', 5));
-    store.dispatch(setProfileAnswer('motivationalFactors', { bikefun: 1 }));
-    store.dispatch(setProfileAnswer('bikeReasons', { 8: true }));
-    store.dispatch(
-      setProfileAnswer('bikeReasons', { 8: true, '8-input': 'test' })
-    );
-    store.dispatch(setZipcode('1', ''));
-    store.dispatch(setZipcode('10', ''));
-    store.dispatch(setZipcode('105', ''));
-    store.dispatch(setZipcode('1055', ''));
-    store.dispatch(setZipcode('10553', ''));
-    store.dispatch(setProfileAnswer('whyBiking', { safety: true }));
-    await store.dispatch(submitProfile());
-
-    const dispatchedActions = store.getActions();
-    expect(dispatchedActions.map((action) => action.type)).toEqual(
-      expectedActions
-    );
   });
 });
