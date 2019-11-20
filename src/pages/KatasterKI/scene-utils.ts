ed697a6f43f9571f1a6031f4beb3125f68a807c8
moklick <info@moritzklack.com>
2019-11-20 21:37:22 +0100

    style(scene): new buttons, loading state

--- src/pages/KatasterKI/scene-utils.ts
+++ src/pages/KatasterKI/scene-utils.ts
@@ -1,9 +1,9 @@
 import { Answer, Perspective, Section } from './types';
 
-import ButtonIconUnsafe from '~/images/kataster-icons/button-backgrund-1.svg';
-import ButtonIconMostyUnsafe from '~/images/kataster-icons/button-backgrund-2.svg';
-import ButtonIconMostlySafe from '~/images/kataster-icons/button-backgrund-3.svg';
-import ButtonIconSafe from '~/images/kataster-icons/button-backgrund-4.svg';
+import ButtonIconUnsafe from '~/images/kataster-icons/button-background-1.svg';
+import ButtonIconMostyUnsafe from '~/images/kataster-icons/button-background-2.svg';
+import ButtonIconMostlySafe from '~/images/kataster-icons/button-background-3.svg';
+import ButtonIconSafe from '~/images/kataster-icons/button-background-4.svg';
 
 import BikeIcon from '~/images/kataster-icons/icon-transportation-2.svg';
 import PedestrianIcon from '~/images/kataster-icons/icon-transportation-1.svg';
