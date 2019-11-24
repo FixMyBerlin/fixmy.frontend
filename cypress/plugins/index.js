92f025fd12ffa195136bd4bae47384d8a52ba042
Sascha <sascha.picard@googlemail.com>
2019-11-24 21:22:45 +0100

    chore(e2e tests): add script to run tests in chrome

---
+++ cypress/plugins/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+module.exports = (on, config) => {
+
+  // store process env in cypress env,
+  // see https://docs.cypress.io/guides/guides/environment-variables.html#Option-2-cypress-env-json
+  config.env = process.env;
+
+  // modify the way browsers are launched,
+  // see https://docs.cypress.io/api/plugins/browser-launch-api.html#Usage
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') {
+      // immediately open dev tools so we can inspect breakpoint halts
+      // (when we added a "debugger" statement in our code
+      args.push('--auto-open-devtools-for-tabs');
+      return args;
+    }
+  });
+
+  // return config
+  return config;
+};
