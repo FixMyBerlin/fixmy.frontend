b0afbbcc1c43c43aa9722d215b5cf2073a2e192f
Vincent Ahrend <mail@vincentahrend.com>
2019-11-25 18:32:32 +0100

    Fix types and sample instance for schema updates

--- src/pages/KatasterKI/types.ts
+++ src/pages/KatasterKI/types.ts
@@ -9,14 +9,14 @@ export interface ProfileRequest {
   berlinTraffic: {
     [name: string]: boolean;
   };
-  bicycleUse: number;
-  bikeReasons: Array<string>;
+  bicycleUse?: number;
+  bikeReasons?: Array<string>;
   bikeReasonsVar?: string;
   district: string;
   gender: 'm' | 'w' | 'd';
   hasChildren: boolean;
   isTosAccepted: boolean;
-  motivationalFactors: {
+  motivationalFactors?: {
     [name: string]: number;
   };
   perspective: Perspective;
