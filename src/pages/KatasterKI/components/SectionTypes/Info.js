958611a107df379cff190175ea280213385e0ab9
Vincent Ahrend <mail@vincentahrend.com>
2019-11-28 17:48:47 +0100

    Decrease info screen font size for small devices (#262)
    * Fix title screen on first scenegroup
    
    * Change info screen text size on small devices
    
    * Fix responsive css syntax

--- src/pages/KatasterKI/components/SectionTypes/Info.js
+++ src/pages/KatasterKI/components/SectionTypes/Info.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { media } from '~/styles/utils';
 import Flex from '~/components/Flex';
 import Button from '~/pages/KatasterKI/components//Button';
 
 const Headline = styled.h1`
-  font-size: 40px;
+  font-size: 28px;
   flex-grow: 1;
   line-height: 1.3;
   font-family: 'Franklin Gothic FS', 'Open Sans', sans-serif;
   font-weight: 500;
   margin-top: 0;
   hyphens: auto;
+
+  ${media.m`
+    font-size: 40px;
+  `}
 `;
 
 export default ({ title, next }) => {
