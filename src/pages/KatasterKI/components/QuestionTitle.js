2241e6639e81f0654e7a71e7034565df556c1be6
Heiko <39826825+Hejco@users.noreply.github.com>
2019-11-28 12:09:08 +0100

    Update QuestionTitle.js

--- src/pages/KatasterKI/components/QuestionTitle.js
+++ src/pages/KatasterKI/components/QuestionTitle.js
@@ -5,12 +5,12 @@ import styled from 'styled-components';
 import { isSmallScreen } from '~/styles/utils';
 
 const HeadlineWrapper = styled.div`
-  margin-bottom: 8px;
+  margin-bottom: 24px;
 
   h1 {
     font-size: 24px;
     line-height: 1.3;
-    margin: 3px 0;
+    margin: 5px 0;
     font-family: 'Franklin Gothic FS', 'Open Sans', sans-serif;
     font-weight: 500;
   }
