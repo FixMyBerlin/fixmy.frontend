954aa42bb6f4c563cd654bddca601df43617d72e
Vincent Ahrend <mail@vincentahrend.com>
2019-12-02 15:30:20 +0100

    Refactor propTypes and defaultProps to be outside class

--- src/components/AutocompleteGeocoder/index.js
+++ src/components/AutocompleteGeocoder/index.js
@@ -1,56 +1,18 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
+import logger from '~/utils/logger';
 import SearchBar from './SearchBar';
 import SuggestionList from './SuggestionList';
 import { fetchSuggestions } from './apiService';
 
 class AutocompleteGeocoder extends PureComponent {
-  static propTypes = {
-    /**
-     * Called when search Input gets Focus.
-     */
-    onInputFocus: PropTypes.func,
-    /**
-     *Called when search Input looses Focus.
-     */
-    onInputBlur: PropTypes.func,
-    /**
-     * Called when the first search String is compiled. Ment to set a flag in the consuming component,
-     * e.g. to hide a Tooltip.
-     */
-    onSearchStart: PropTypes.func,
-    /**
-     * Called with the coordinates object {lat, lng} and address string of the picked location.
-     */
-    onLocationPick: PropTypes.func.isRequired,
-    /**
-     * Error callback.
-     */
-    onError: PropTypes.func.isRequired,
-    /**
-     * Minimum input length that triggers this.props.onSearchEnter()
-     * e.g. to clear the results list.
-     */
-    searchStringMinLength: PropTypes.number,
-    /**
-     * Amount of milliseconds the invokation of this.props.onSearchEnter() isdelayed
-     * since its last invocation.
-     */
-    debounceTime: PropTypes.number
-  };
-
-  static defaultProps = {
-    onInputFocus: () => {},
-    onInputBlur: () => {},
-    onSearchStart: () => {},
-    searchStringMinLength: 3,
-    debounceTime: 1000
-  };
-
-  state = {
-    suggestions: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      suggestions: []
+    };
+  }
 
   clearSuggestions = () => this.setState({ suggestions: [] });
 
@@ -106,5 +68,47 @@ class AutocompleteGeocoder extends PureComponent {
     );
   }
 }
+
+AutocompleteGeocoder.propTypes = {
+  /**
+   * Called when search Input gets Focus.
+   */
+  onInputFocus: PropTypes.func,
+  /**
+   *Called when search Input looses Focus.
+   */
+  onInputBlur: PropTypes.func,
+  /**
+   * Called when the first search String is compiled. Ment to set a flag in the consuming component,
+   * e.g. to hide a Tooltip.
+   */
+  onSearchStart: PropTypes.func,
+  /**
+   * Called with the coordinates object {lat, lng} and address string of the picked location.
+   */
+  onLocationPick: PropTypes.func.isRequired,
+  /**
+   * Error callback.
+   */
+  onError: PropTypes.func.isRequired,
+  /**
+   * Minimum input length that triggers this.props.onSearchEnter()
+   * e.g. to clear the results list.
+   */
+  searchStringMinLength: PropTypes.number,
+  /**
+   * Amount of milliseconds the invokation of this.props.onSearchEnter() isdelayed
+   * since its last invocation.
+   */
+  debounceTime: PropTypes.number
+};
+
+AutocompleteGeocoder.defaultProps = {
+  onInputFocus: () => {},
+  onInputBlur: () => {},
+  onSearchStart: () => {},
+  searchStringMinLength: 3,
+  debounceTime: 1000
+};
 
 export default AutocompleteGeocoder;
