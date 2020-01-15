5e3f4eee1a2bb2536e053a8e80b08e07a8fb5e87
Vincent Ahrend <mail@vincentahrend.com>
2020-01-15 11:54:12 +0100

    Fix missing HBI icons

--- src/pages/Map/config/hbi.ts
+++ src/pages/Map/config/hbi.ts
@@ -8,29 +8,25 @@ const stops = [
     min: 0,
     max: 2.5,
     color: 'hsl(22, 100%, 52%)',
-    label: 'sehr schlecht',
-    image: 'sehrgefaehrlich@2x.png'
+    label: 'sehr schlecht'
   },
   {
     min: 2.5,
     max: 5,
     color: 'hsl(14, 83%, 74%)',
-    label: 'schlecht',
-    image: 'gefaehrlich@2x.png'
+    label: 'schlecht'
   },
   {
     min: 5,
     max: 7.5,
     color: '#a0ebe3',
-    label: 'ok',
-    image: 'ok@2x.png'
+    label: 'ok'
   },
   {
     min: 7.5,
     max: 100,
     color: 'hsl(174, 87%, 43%)',
-    label: 'super',
-    image: 'sehrgut@2x.png'
+    label: 'super'
   }
 ];
 
