b566c5e4876e0c2bdedfd3c978a34e92f7d27ff6
Vincent Ahrend <mail@vincentahrend.com>
2020-02-06 17:43:51 +0100

    Add API config for sample new region

--- src/config/bonn/index.ts
+++ src/config/bonn/index.ts
@@ -2,9 +2,18 @@ import map from './map';
 import menu from './menu';
 import routes from './routes';
 
+const apiEndpoints = {
+  dev: 'http://localhost:8000/api',
+  staging: 'https://elegant-hugle-53926c.netlify.com/api/bonn/next'
+};
+
 export default {
   map,
   menu,
   routes,
-  siteTitle: 'FixMyBonn'
+  siteTitle: 'FixMyBonn',
+  apiUrl:
+    process.env.API_URL ||
+    apiEndpoints[process.env.CONFIG_ENV] ||
+    apiEndpoints.staging
 };
