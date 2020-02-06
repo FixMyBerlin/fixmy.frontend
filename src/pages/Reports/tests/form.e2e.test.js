afe9da793abd3a8e441b06fcb60f6fce47698cfd
Vincent Ahrend <mail@vincentahrend.com>
2020-02-06 17:10:59 +0100

    Extend form tests

--- src/pages/Reports/tests/form.e2e.test.js
+++ src/pages/Reports/tests/form.e2e.test.js
@@ -3,9 +3,10 @@ import config from '~/pages/Reports/config';
 
 describe('The reports submission form', () => {
   describe('starting page', () => {
-    before(() => {
+    beforeEach(() => {
       cy.visit(config.routes.reports.new);
     });
+
     it('has a header', () => {
       getByDataAttr`reports-heading`.should('be.visible');
     });
@@ -15,6 +16,7 @@ describe('The reports submission form', () => {
     it('has a title', () => {
       getByDataAttr`reports-locatemode-title`.should('be.visible');
     });
+
     describe('the locate me button', () => {
       it('is visible', () => {
         getByDataAttr`reports-locatemode-currentPosition`.should('be.visible');
@@ -22,13 +24,25 @@ describe('The reports submission form', () => {
       it('redirects to the map', () => {
         cy.mockGeolocation();
         getByDataAttr`reports-locatemode-currentPosition`.click();
+        cy.window().then((win) => {
+          // disabled because chai uses this syntax
+          // eslint-disable-next-line no-unused-expressions
+          expect(win.navigator.geolocation.getCurrentPosition).to.be.called;
+        });
         getByDataAttr`reports-locateme-address-invalid`.should('be.visible');
-        cy.visit(config.routes.reports.new);
       });
     });
-    it('shows an address-input button', () => {
-      getByDataAttr`reports-locatemode-enterPosition`.should('be.visible');
+
+    describe('the address-input button', () => {
+      it('should be visible', () => {
+        getByDataAttr`reports-locatemode-enterPosition`.should('be.visible');
+      });
+      it('should take users to the map when clicked', () => {
+        getByDataAttr`reports-locatemode-enterPosition`.click();
+        cy.url().should('be', config.routes.reports.map);
+      });
     });
+
     describe('the close button', () => {
       it('is visible', () => {
         getByDataAttr`reports-locatemode-close-button`.should('be.visible');
