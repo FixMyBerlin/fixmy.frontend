05ac29ef6efc89abb50f557b2d7c1b292d752de2
Vincent Ahrend <mail@vincentahrend.com>
2020-02-11 14:06:33 +0100

    Make Cypress Chrome window position configurable

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,29 @@
 const wp = require('@cypress/webpack-preprocessor');
 const webpackOptions = require('../../webpack/webpack.config.dev.js');
+const cypressConfig = require('../../cypress.json');
+
+// immediately open dev tools so we can inspect breakpoint halts
+// (when we added a "debugger" statement in our code
+const setAutoDevTools = (args) => args.push('--auto-open-devtools-for-tabs');
+
+// Set Chrome's window position and size
+const setWindowPos = (args) => {
+  args.push(
+    '--user-data-dir="~/chrome-test-user"',
+    `--window-size=${cypressConfig.chrome.windowSize}`,
+    `--window-position=${cypressConfig.chrome.windowPosition}`
+  );
+};
 
 module.exports = (on, config) => {
   // modify the way browsers are launched,
   // see https://docs.cypress.io/api/plugins/browser-launch-api.html#Usage
   on('before:browser:launch', (browser = {}, args) => {
     if (browser.name === 'chrome') {
-      // immediately open dev tools so we can inspect breakpoint halts
-      // (when we added a "debugger" statement in our code
-      args.push('--auto-open-devtools-for-tabs');
-      // TODO: pause on exception (if there is such cli arg)
-      return args;
+      setAutoDevTools(args);
+      setWindowPos(args);
     }
+    return args;
   });
 
   on('file:preprocessor', wp({ webpackOptions }));
