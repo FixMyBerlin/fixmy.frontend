eec713dd250252c6fc3e533475f84fbe059fc281
Vincent Ahrend <mail@vincentahrend.com>
2020-02-19 17:38:42 +0100

    Clarify meaning of CONFIG_ENV by renaming to BACKEND

--- src/config/bonn/index.ts
+++ src/config/bonn/index.ts
@@ -3,7 +3,7 @@ import menu from './menu';
 import routes from './routes';
 
 const apiEndpoints = {
-  dev: 'http://localhost:8000/api',
+  local: 'http://localhost:8000/api',
   staging: 'https://elegant-hugle-53926c.netlify.com/api/bonn/next'
 };
 
