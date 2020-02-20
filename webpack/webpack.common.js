6172fce5a9d698d5d59719df5888c87e8f637f7a
Vincent Ahrend <mail@vincentahrend.com>
2020-02-20 16:18:45 +0100

    Revert "Revert limiting chunk splitting"
    This reverts commit 52b82fd31fddd573f780deb3b07db749f3d54650.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const defaultBaseName = '/';
+const defaultEntryPoint = '../src/index.js';
 
 const FAVICONS_PATH =
   process.env.KATASTER_PATH != null
@@ -12,11 +13,7 @@ const FAVICONS_PATH =
 
 module.exports = {
   entry: {
-    App: Path.resolve(__dirname, '../src/index.js'),
-    Analysis: Path.resolve(__dirname, '../src/pages/Analysis/index.js'),
-    KatasterKI: Path.resolve(__dirname, '../src/pages/KatasterKI/index.js'),
-    Plannings: Path.resolve(__dirname, '../src/pages/Map/index.js'),
-    Reports: Path.resolve(__dirname, '../src/pages/Reports/index.js')
+    app: Path.resolve(__dirname, process.env.ENTRY_POINT || defaultEntryPoint)
   },
   output: {
     path: Path.join(__dirname, '../build'),
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -25,23 +25,7 @@ module.exports = merge(common, {
   optimization: {
     splitChunks: {
       chunks: 'all',
-      name: false,
-      maxInitialRequests: Infinity,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          }
-        }
-      }
+      name: false
     },
     minimizer: [new TerserPlugin()]
   },
@@ -56,7 +40,10 @@ module.exports = merge(common, {
       minify: false
     }),
     new MiniCssExtractPlugin({ filename: 'bundle.css' }),
-    new Webpack.optimize.ModuleConcatenationPlugin()
+    new Webpack.optimize.ModuleConcatenationPlugin(),
+    new Webpack.optimize.MinChunkSizePlugin({
+      minChunkSize: 10000
+    })
   ],
   module: {
     rules: [
