3503b8a2a65eb507854818501f11caa5fc779a06
Sascha <sascha.picard@googlemail.com>
2020-02-27 20:55:04 +0100

    update request method signature

--- src/services/http.js
+++ src/services/http.js
@@ -3,25 +3,31 @@ import { isFunction } from '~/utils/utils';
 
 /**
  *
- * @param {String} route A relative path within our REST-API.
+ * @param {String} route An absolute path.
  * @param {String} [method=get] The http method to be used.
  * One of get, post, put, patch, head and delete.
  * @param {Object} [json] An optional JSON payload
  * @param {String} [token] An optional JWT token to authenticate the user.
- * @param {Function} [setSubmitting] If passed, this function will be called
+ * @param {hooks} Callbacks functions.
+ * @param {Function} [hooks.setSubmitting] If passed, this function will be called
  * with true before the request and with false after the request.
- * @param {Function} [setErrors} If passed, after a request has failed
+ * @param {Function} [hooks.setErrors} If passed, after a request has failed
  * this function will be called with a meaningful error provided by our API
  * @param [respType=json] The response type
  * @returns {Promise<void>}
+ * @throws Will throw with either a qualified error provided under e.response.json
  */
 async function request(
   route,
-  { method = 'POST', json = {}, token = false },
+  method = 'get',
+  json,
+  token,
   { setSubmitting, setErrors },
   respType = 'json'
 ) {
-  const [useSetSubmitting, useSetErrors] = [setSubmitting, setErrors].map(isFunction);
+  const [useSetSubmitting, useSetErrors] = [setSubmitting, setErrors].map(
+    isFunction
+  );
 
   let response;
   if (useSetSubmitting) {
@@ -32,7 +38,6 @@ async function request(
 
   try {
     response = await ky(route, { method, json, headers });
-
     if (respType) {
       response = await response[respType]();
     }
@@ -51,16 +56,14 @@ async function request(
   return response;
 }
 
-export async function get(route, payloadOptions, ...args) {
-  const mergedOptions = { ...payloadOptions, method: 'get' };
-  return request(route, mergedOptions, ...args);
+export async function get(route, method, ...args) {
+  return request(route, 'get', ...args);
 }
 
-export async function post(...args) {
-  return request(...args);
+export async function post(route, method, ...args) {
+  return request(route, 'post', ...args);
 }
 
-export async function patch(route, payloadOptions, ...args) {
-  const mergedOptions = { ...payloadOptions, method: 'patch' };
-  return request(route, mergedOptions, ...args);
+export async function patch(route, method, ...args) {
+  return request(route, 'patch', ...args);
 }
