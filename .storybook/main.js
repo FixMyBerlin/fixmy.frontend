d7a76e4b76e84181f42e7838717ae32a1ba0eecf
Vincent Ahrend <mail@vincentahrend.com>
2020-03-12 19:51:25 +0100

    Fix config for loading SVGS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,48 @@
 const merge = require('webpack-merge');
 const common = require('../webpack/webpack.common.js');
+const path = require('path');
+
+const AppSourceDir = path.join(__dirname, '..', 'src');
+
+/**
+ * Replace Storybook's malfuncioning SVG rule with our working one
+ *
+ * @param {Object} config with non-working SVG rule
+ */
+const replaceSvgRule = (config) => {
+  const svgLoader = {
+    use: [
+      'babel-loader',
+      {
+        loader: 'react-svg-loader',
+        options: {
+          svgo: {
+            plugins: [{ cleanupIDs: false }, { removeViewBox: false }]
+          }
+        }
+      }
+    ]
+  };
+
+  const svgRule = config.module.rules.find((rule) =>
+    'test.svg'.match(rule.test)
+  );
+  svgRule.exclude = [AppSourceDir];
+
+  config.module.rules.push({
+    test: /\.svg$/i,
+    include: [AppSourceDir],
+    use: [svgLoader]
+  });
+  return config;
+};
 
 module.exports = {
   stories: ['../src/**/*.stories.[tj]s'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: (config) => merge(common, config)
+  webpackFinal: (config) => {
+    const merged = merge(common, config);
+    const svgFixed = replaceSvgRule(config);
+    return merged;
+  }
 };
