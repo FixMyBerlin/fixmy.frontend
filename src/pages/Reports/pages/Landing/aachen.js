552dd7db579c65970b84bd7d5db9584a66278a65
Vincent Ahrend <mail@vincentahrend.com>
2020-03-12 14:20:20 +0100

    Add quote to Aachen preview

--- src/pages/Reports/pages/Landing/aachen.js
+++ src/pages/Reports/pages/Landing/aachen.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import JoinButton from './components/JoinButton';
 import HowItWorksSection from './components/HowItWorksSecion';
+import Quote from './components/QuoteAachen';
 import MapLink from './components/MapLink';
 import Faq from './components/Faq';
 import HorizontalRuler from '~/pages/Reports/pages/SubmitReport/components/HorizontalRuler';
@@ -10,6 +11,7 @@ const BerlinLanding = () => (
   <>
     <HowItWorksSection />
     <HorizontalRuler className="light" />
+    <Quote />
     <JoinButton />
     <MapLink />
     <HorizontalRuler className="light" />
--- src/pages/Reports/pages/Landing/berlin.js
+++ src/pages/Reports/pages/Landing/berlin.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import JoinButton from './components/JoinButton';
 import HowItWorksSection from './components/HowItWorksSecion';
-import Quote from './components/QuoteSection';
+import Quote from './components/QuoteWeisbrich';
 import MapLink from './components/MapLink';
 import Faq from './components/Faq';
 import HorizontalRuler from '~/pages/Reports/pages/SubmitReport/components/HorizontalRuler';
