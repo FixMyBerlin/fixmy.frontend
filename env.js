007813a9d201bd07feb1bb57db4075faff4ef1db
Sascha <sascha.picard@googlemail.com>
2020-03-15 19:44:55 +0100

    use nps to override list of (ignored) e2e tests

---
+++ env.js
@@ -0,0 +1,14 @@
+// central place to read and expose env vars and to assign default values
+// TODO: consider move other references of process.env to here
+
+// read env vars
+const result = require('dotenv').config();
+
+if (result.error) {
+  throw result.error
+}
+const env = result.parsed;
+
+module.exports = {
+  region: env.REGION || 'bonn'
+}
