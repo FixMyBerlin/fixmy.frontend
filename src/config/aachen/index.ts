b8b23d5e4f5410ce00d9afb3c2990629f44ebd60
Vincent Ahrend <mail@vincentahrend.com>
2020-03-17 13:27:29 +0100

    Fix backend selection

--- src/config/aachen/index.ts
+++ src/config/aachen/index.ts
@@ -15,6 +15,6 @@ export default {
   siteTitle: 'FixMyAachen',
   apiUrl:
     process.env.API_URL ||
-    apiEndpoints[process.env.CONFIG_ENV] ||
+    apiEndpoints[process.env.BACKEND] ||
     apiEndpoints.staging
 };
