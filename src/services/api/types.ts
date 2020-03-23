2f996ddb3c48ae0aff27d24a9d6daa19e119a9b3
geomC <sascha.picard@gmail.com>
2020-03-23 21:46:55 +0100

    refactor(type): prettify

--- src/services/api/types.ts
+++ src/services/api/types.ts
@@ -3,24 +3,24 @@ import { Options as KyOptions } from 'ky';
 export type JSONPrimitive = string | number | boolean | null;
 export type JSONObject = { [member: string]: JSONValue };
 
-export interface JSONArray extends Array<JSONValue> {
-}
+export interface JSONArray extends Array<JSONValue> {}
 
 export type JSONValue = JSONPrimitive | JSONObject | JSONArray;
 
 export type Callbacks = {
   setSubmitting?: (boolean) => void;
   setErrors?: (JsonValue) => void;
-}
+};
 
-export type ResponseBodyType = 'arrayBuffer' |
-  'blob' |
-  'formData' |
-  'json' |
-  'text'
+export type ExpectedResponseBodyType =
+  | 'arrayBuffer'
+  | 'blob'
+  | 'formData'
+  | 'json'
+  | 'text';
 
 export type RequestOptions = {
   kyOptions?: KyOptions;
   callbacks?: Callbacks;
-  responseBodyType?: ResponseBodyType;
+  responseBodyType?: ExpectedResponseBodyType;
 };
