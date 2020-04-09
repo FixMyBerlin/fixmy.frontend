7d8692562a06c1ca23376c1b31a25cea43fad54e
Vincent Ahrend <mail@vincentahrend.com>
2020-04-09 16:54:50 +0200

    Update marker images

--- src/pages/Reports/utils.ts
+++ src/pages/Reports/utils.ts
@@ -4,13 +4,13 @@ import config from '~/pages/Reports/config';
 import AachenMarkerNew from '~/images/reports/marker/aachen/pina-meldung@2x.png';
 import AachenMarkerVerification from '~/images/reports/marker/aachen/meldung-circle-copy-4@2x.png';
 import AachenMarkerAccepted from '~/images/reports/marker/aachen/meldung-circle@2x.png';
-// import AachenMarkerRejected from '~/images/reports/marker/aachen/pin-meldung-klein-fertig-copy@2x.png';
-// import AachenMarkerDone from '~/images/reports/marker/aachen/pin-meldung-klein-fertig@2x.png';
+import AachenMarkerRejected from '~/images/reports/marker/aachen/rejected.png';
+import AachenMarkerDone from '~/images/reports/marker/aachen/done.png';
 
 import BerlinMarkerVerification from '~/images/reports/marker/default/pin-verification.png';
-import BerlinMarkerAccepted from '~/images/reports/marker/default/pin-accepted.png';
-import BerlinMarkerRejected from '~/images/reports/marker/default/pin-rejected.png';
-import BerlinMarkerDone from '~/images/reports/marker/default/pin-done.png';
+// import BerlinMarkerAccepted from '~/images/reports/marker/default/pin-accepted.png';
+// import BerlinMarkerRejected from '~/images/reports/marker/default/pin-rejected.png';
+// import BerlinMarkerDone from '~/images/reports/marker/default/pin-done.png';
 
 // The "new" marker is not used anymore and replaced by the verification marker
 // for now
@@ -19,15 +19,15 @@ const ICONS_BY_STATUS = {
     new: AachenMarkerNew,
     verification: AachenMarkerVerification,
     accepted: AachenMarkerAccepted,
-    rejected: AachenMarkerNew,
-    done: AachenMarkerNew
+    rejected: AachenMarkerRejected,
+    done: AachenMarkerDone
   },
   default: {
     new: BerlinMarkerVerification,
     verification: AachenMarkerVerification,
-    accepted: BerlinMarkerAccepted,
-    rejected: BerlinMarkerRejected,
-    done: BerlinMarkerDone
+    accepted: AachenMarkerAccepted,
+    rejected: AachenMarkerRejected,
+    done: AachenMarkerDone
   }
 };
 
