571ea69ac4a7009eed738cd3755e5e8d2d75942d
Vincent Ahrend <mail@vincentahrend.com>
2020-04-15 12:16:46 +0200

    Add new report statuses and update marker images (#396)
    * Add translations for new statuses
    
    * Add new marker images
    
    * Update styles
    
    * Handle prefixed marker statuses

--- src/pages/Reports/utils.ts
+++ src/pages/Reports/utils.ts
@@ -1,33 +1,58 @@
 import logger from '~/utils/logger';
 import config from '~/pages/Reports/config';
 
-import AachenMarkerNew from '~/images/reports/marker/aachen/pina-meldung@2x.png';
-import AachenMarkerVerification from '~/images/reports/marker/aachen/meldung-circle-copy-4@2x.png';
-import AachenMarkerAccepted from '~/images/reports/marker/aachen/meldung-circle@2x.png';
-import AachenMarkerRejected from '~/images/reports/marker/aachen/rejected.png';
-import AachenMarkerDone from '~/images/reports/marker/aachen/done.png';
+import AachenMarkerNew from '~/images/reports/marker/aachen/new@2x.png';
+import AachenMarkerVerification from '~/images/reports/marker/aachen/verification@2x.png';
+import AachenMarkerAccepted from '~/images/reports/marker/aachen/accepted@2x.png';
+import AachenMarkerRejected from '~/images/reports/marker/aachen/rejected@2x.png';
+import AachenMarkerInactive from '~/images/reports/marker/aachen/inactive@2x.png';
+import AachenMarkerPlanning from '~/images/reports/marker/aachen/planning@2x.png';
+import AachenMarkerExecution from '~/images/reports/marker/aachen/execution@2x.png';
+import AachenMarkerInvalid from '~/images/reports/marker/aachen/invalid@2x.png';
+import AachenMarkerDone from '~/images/reports/marker/aachen/done@2x.png';
 
-import BerlinMarkerVerification from '~/images/reports/marker/default/pin-verification.png';
-// import BerlinMarkerAccepted from '~/images/reports/marker/default/pin-accepted.png';
-// import BerlinMarkerRejected from '~/images/reports/marker/default/pin-rejected.png';
-// import BerlinMarkerDone from '~/images/reports/marker/default/pin-done.png';
+import DefaultMarkerNew from '~/images/reports/marker/default/new@2x.png';
+import DefaultMarkerVerification from '~/images/reports/marker/default/verification@2x.png';
+import DefaultMarkerAccepted from '~/images/reports/marker/default/accepted@2x.png';
+import DefaultMarkerRejected from '~/images/reports/marker/default/rejected@2x.png';
+import DefaultMarkerInactive from '~/images/reports/marker/default/inactive@2x.png';
+import DefaultMarkerPlanning from '~/images/reports/marker/default/planning@2x.png';
+import DefaultMarkerExecution from '~/images/reports/marker/default/execution@2x.png';
+import DefaultMarkerInvalid from '~/images/reports/marker/default/invalid@2x.png';
+import DefaultMarkerDone from '~/images/reports/marker/default/done@2x.png';
 
-// The "new" marker is not used anymore and replaced by the verification marker
-// for now
 const ICONS_BY_STATUS = {
   aachen: {
+    report_new: AachenMarkerNew,
+    report_verification: AachenMarkerVerification,
+    report_accepted: AachenMarkerAccepted,
+    report_rejected: AachenMarkerRejected,
+    report_inactive: AachenMarkerInactive,
     new: AachenMarkerNew,
     verification: AachenMarkerVerification,
     accepted: AachenMarkerAccepted,
     rejected: AachenMarkerRejected,
+    inactive: AachenMarkerInactive,
+    planning: AachenMarkerPlanning,
+    execution: AachenMarkerExecution,
+    invalid: AachenMarkerInvalid,
     done: AachenMarkerDone
   },
   default: {
-    new: BerlinMarkerVerification,
-    verification: AachenMarkerVerification,
-    accepted: AachenMarkerAccepted,
-    rejected: AachenMarkerRejected,
-    done: AachenMarkerDone
+    report_new: DefaultMarkerNew,
+    report_verification: DefaultMarkerVerification,
+    report_accepted: DefaultMarkerAccepted,
+    report_rejected: DefaultMarkerRejected,
+    report_inactive: DefaultMarkerInactive,
+    new: DefaultMarkerNew,
+    verification: DefaultMarkerVerification,
+    accepted: DefaultMarkerAccepted,
+    rejected: DefaultMarkerRejected,
+    inactive: DefaultMarkerInactive,
+    planning: DefaultMarkerPlanning,
+    execution: DefaultMarkerExecution,
+    invalid: DefaultMarkerInvalid,
+    done: DefaultMarkerDone
   }
 };
 
