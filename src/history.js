1b0d31cc01b93af138f7b9e250a87ced8f0ea75f
Vincent Ahrend <mail@vincentahrend.com>
2020-04-15 13:22:44 +0200

    Fix issue with react-piwik-router in Jest env

--- src/history.js
+++ src/history.js
@@ -4,19 +4,40 @@ import { createBrowserHistory } from 'history';
 import config from '~/config';
 
 const piwikSiteId = config.piwik.siteId[config.region] || 1;
-const piwik = new ReactPiwik({
-  url: config.piwik.url,
-  siteId: piwikSiteId,
-  ...config.piwik.options
+
+const browserHistory = createBrowserHistory({
+  basename: process.env.BASE_NAME
 });
 
-const history = piwik.connectToHistory(
-  createBrowserHistory({ basename: process.env.BASE_NAME })
-);
+// piwik-react-router has an open issue where it will try and attach a
+// script tag in the dom, which doesn't work in a Jest environment.
+// As a workaround, the resulting error is caught here.
+// See https://github.com/joernroeder/piwik-react-router/issues/48
+
+let history;
+try {
+  const piwik = new ReactPiwik({
+    url: config.piwik.url,
+    siteId: piwikSiteId,
+    ...config.piwik.options
+  });
+
+  history = piwik.connectToHistory(browserHistory);
+} catch (e) {
+  if (e.message === "Cannot read property 'parentNode' of undefined") {
+    history = browserHistory;
+  } else {
+    throw e;
+  }
+}
 
 // used in integration tests
 if (window.Cypress) {
   window.appHistory = history;
 }
 
-export default history;
+// Export history as a constant, even though it was mutable before
+// to handle exception
+const constHistory = history;
+
+export default constHistory;
