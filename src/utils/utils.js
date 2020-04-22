9a9fe0a6d4520100da9d0e12245eb40e29bd291a
s-pic <sascha.picard@gmail.com>
2020-04-22 21:08:07 +0200

    refactor(utils.js): remove unused isFunction util

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -72,9 +72,3 @@ export function getRVALength({ length, side, id }) {
 }
 
 export const isTouch = 'ontouchstart' in window;
-
-export function isFunction(functionToCheck) {
-  return (
-    functionToCheck && {}.toString.call(functionToCheck) === '[object Function]'
-  );
-}
