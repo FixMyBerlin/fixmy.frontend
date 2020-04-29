53521a3baf2e04191ec64fa4f72a0d1824ad5a44
Vincent Ahrend <mail@vincentahrend.com>
2020-04-29 18:11:15 +0200

    Fix typo

--- src/pages/Spielstrassen/config.ts
+++ src/pages/Spielstrassen/config.ts
@@ -61,7 +61,7 @@ export default {
       },
       {
         street: 'Liebigstraße',
-        kiez: 'Samarieterviertel',
+        kiez: 'Samariterviertel',
         region: 'Friedrichshain'
       }
     ]
