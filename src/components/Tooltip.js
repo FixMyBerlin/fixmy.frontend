dae1bccc01701d50ef54eff06cd34396d7998ead
Vincent Ahrend <mail@vincentahrend.com>
2020-05-07 13:17:37 +0200

    Remove unused tooltip libraries and component

--- src/components/Tooltip.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import ReactTooltip from 'react-tooltip';
-
-export default styled(ReactTooltip)`
-  width: 275px;
-  border-radius: 2px;
-  line-height: 1.4;
-`;
