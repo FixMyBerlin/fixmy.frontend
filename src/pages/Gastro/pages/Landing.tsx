32ae7d6713419237f0931572ed09a49b12e56815
Heiko <39826825+Hejco@users.noreply.github.com>
2020-05-14 08:52:01 +0200

    Update Landing.tsx

--- src/pages/Gastro/pages/Landing.tsx
+++ src/pages/Gastro/pages/Landing.tsx
@@ -45,7 +45,7 @@ const Landing = () => {
     <>
       <Header showInfoLink={false} />
       <StyledContainer maxWidth="md">
-        <h1>X-Hain isst draußen – Offene Straßen für Viele</h1>
+        <h1>XHain is(s)t draußen – Terassen für Vieles</h1>
         <ImageInsert
           src={BackgroundImageA2}
           srcSet={`${BackgroundImageA1} 450w, ${BackgroundImageA2} 750w, ${BackgroundImageA3} 1125w`}
@@ -60,11 +60,11 @@ const Landing = () => {
             Photo by Freddy Do on Unsplash
           </a>
         </Attribution>
+        <h2>Bedarfsmeldung für Gastronomie, Einzelhandel und soziale Projekte</h2>
         <p>
           Das Bezirksamt Friedrichshain-Kreuzberg möchte Gastronomiebetrieben,
-          Einzelhandel und sozialen Projekte die Möglichkeit bieten, zusätzliche
-          Flächen im Straßenraum zum Aufstellen von Tischen zu nutzen. So können
-          diese die Vorgaben der Corona-Eindämmungsverordnung umsetzen und
+          Einzelhandel und sozialen Projekten, die gemäß §6 der Sechsten Verordnung zur Änderung der SARS-Cov-2-Eindämmungsmaßnahmenverordnung vom 07. Mai 2020  wieder für den Publikumsverkehr öffnen dürfen, die Möglichkeit bieten, Flächen im Straßenraum zum Aufstellen von Tischen zu nutzen. 
+          So können diese die Vorgaben der Corona-Eindämmungsverordnung umsetzen und
           dennoch Ihren Betrieb wieder aufzunehmen.
         </p>
         <p>
@@ -88,21 +88,20 @@ const Landing = () => {
           Das Bezirksamt Friedrichshain-Kreuzberg bietet in dieser Situation
           Gewerbetrieben, Einzelhandel und sozialen Projekte an, Tische, Stühle
           und Auslagen temporär auf das Straßenland zu verlagern. Melden Sie
-          über dieses Formular bis zum Sonntag Ihren Bedarf an. Das Bezirksamt
-          wertet Ihre Bedarfsmeldungen innerhalb einer Woche aus und wird auf
+          über dieses Formular bis zum Sonntag den 17.05.2020 Ihren Bedarf an. Das Bezirksamt
+          wertet Ihre Bedarfsmeldungen aus und wird auf
           dieser Grundlage entsprechende Anordnungen vorbereiten.
         </p>
-        <h2>Welche Flächen können genutzt werden?</h2>
+        <h2>Welche Flächen können wann genutzt werden?</h2>
         <p>
-          Je nach Dichte der Bedarfsmeldungen können Flächen im ruhenden
-          Parkraum oder auf der Fahrbahn angeordnet werden. Das Stellen von
+          Nach ca. einwöchiger Prüfung der Bedarfsanmeldungen wird das Bezirksamt entsprechende verkehrsrechtliche Anordnungen treffen. Die Genehmigungen werden mit Blick auf die Eindämmungsmaßnahmen befristet und bis auf weiteres gebührenfrei ausgesprochen. Je nach Aufkommen der Bedarfsmeldungen können Flächen im ruhenden
+          Parkraum oder auf der Fahrbahn angeordnet werden. Zeitlich werden dabei zunächst Freitag, Samstag, Sonntag, jeweils von 11:00 Uhr bis 22:00 Uhr, Flächen als zusätzliche Außenflächen temporär angeboten. Das Stellen von
           Tischen, Stühlen und Auslagen auf der Fahrbahn wird nur möglich sein,
           wenn im entsprechenden Straßenabschnitt mehrere Gewerbetreibende und
           Projekte Bedarf angemeldet haben, sodass die Sperrung der Straße
           temporär von Freitag vormittag bis Sonntag Abend für den motorisierten
           Individualverkehr gerechtfertigt werden kann. Alternativ ist in wenig
-          befahrenen Straßen das Stellen auf dem Parkstreifen, analog zum Wiener
-          Modell mögich.{' '}
+          befahrenen Straßen das Stellen auf dem Parkstreifen möglich.{' '}
         </p>
         <h2>Bedingungen für die Genehmigung</h2>
         <p>
@@ -112,14 +111,14 @@ const Landing = () => {
         <ol>
           <li>
             Eigenverantwortliche Durchführung der verkehrsrechtlichen Anordnung,
-            incl. Stellung von Sperren, Schildern und ggf. Ordner-Personal
+            inkl. Stellung von Sperren, Schildern und ggf. Personal
           </li>
           <li>
             Verpflichtung zur Einführung eines Pfandsystems für Einweggebinde
             bei der Herausgabe von Speisen nach Maßgabe des Bezirksamtes
           </li>
           <li>
-            Freihalten von ausreichend breiten Gehwegen (Mindestens 2 Meter)
+            Freihaltung von ausreichend breiten Gehwegen (Mindestens 2 Meter)
           </li>
         </ol>
         <h2>Grund für die Maßnahme</h2>
