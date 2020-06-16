d35005c1a4717df26ea134ddc5700a9e1585ec20
Vincent Ahrend <mail@vincentahrend.com>
2020-06-16 18:55:41 +0200

    Let AreaMap handle zone regulation without exception

--- src/pages/Gastro/components/AreaMap.tsx
+++ src/pages/Gastro/components/AreaMap.tsx
@@ -12,37 +12,44 @@ const StyledMap = styled(Map)`
 
 const AreaMap = ({ application, printable = false }) => {
   const { geometry, area } = application;
-  const addAreaLayer = (map) => {
-    map.addSource('usageArea', {
-      type: 'geojson',
-      data: {
-        type: 'Feature',
-        geometry: area
-      }
-    });
-    map.addLayer({
-      id: 'usageArea',
-      type: 'fill',
-      source: 'usageArea',
-      layout: {},
-      paint: {
-        'fill-color': config.colors.change_4,
-        'fill-opacity': 0.8
-      }
-    });
+  const handleMapInit = (map) => {
     map.setCenter(geometry?.coordinates);
 
-    const bounds = area.coordinates[0].reduce(
-      (b: MapboxGL.LngLatBounds, coords: [number, number]) => b.extend(coords),
-      new MapboxGL.LngLatBounds(area.coordinates[0][0], area.coordinates[0][0])
-    );
+    if (area != null) {
+      map.addSource('usageArea', {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          geometry: area
+        }
+      });
+      map.addLayer({
+        id: 'usageArea',
+        type: 'fill',
+        source: 'usageArea',
+        layout: {},
+        paint: {
+          'fill-color': config.colors.change_4,
+          'fill-opacity': 0.8
+        }
+      });
 
-    map.fitBounds(bounds, { padding: 20, maxZoom: 17.5, linear: true });
+      const bounds = area.coordinates[0].reduce(
+        (b: MapboxGL.LngLatBounds, coords: [number, number]) =>
+          b.extend(coords),
+        new MapboxGL.LngLatBounds(
+          area.coordinates[0][0],
+          area.coordinates[0][0]
+        )
+      );
+
+      map.fitBounds(bounds, { padding: 20, maxZoom: 17.5, linear: true });
+    }
   };
 
   return (
     <StyledMap
-      onInit={addAreaLayer}
+      onInit={handleMapInit}
       style={config.gastro.map.style}
       bounds={config.gastro.map.bounds}
       interactive={false}
