1fe72b230eddf0333467ebc72f6c1acd07b81063
s-pic <sascha.picard@gmail.com>
2020-07-02 21:54:56 +0200

    fix(components2&Link): long links break layout

--- src/components2/Link/index.tsx
+++ src/components2/Link/index.tsx
@@ -16,6 +16,9 @@ const BaseContainer = styled.a`
   background-repeat: no-repeat;
   background-position: center left;
   padding-left: 15px;
+  
+  /* make sure long anchors wrap */
+  white-space: pre-wrap;
 `;
 
 const Internal = styled(BaseContainer)`
