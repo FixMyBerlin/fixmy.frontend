cc854f23a412d755c1d77ebe1f83231c882f691e
Boris Hekele <boris@fixmyberlin.de>
2020-07-05 23:22:23 +0200

    Fix conflict

--- src/pages/Research/sections/04_dataset.tsx
+++ src/pages/Research/sections/04_dataset.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Heading, Paragraph } from '~/components2/Article';
 import Link from '~/components2/Link';
-<<<<<<< HEAD
-=======
 import { AnchorButton } from '~/components2/Button';
 import ButtonWrapper from '../components/ButtonWrapper';
 import DownloadInfo from '~/pages/Research/components/DownloadInfo';
->>>>>>> origin/develop
 
 const SectionDataset = ({ toc }) => (
   <>
@@ -18,22 +15,6 @@ const SectionDataset = ({ toc }) => (
       unter der Bedingung, dass abgeleitete Werke auch veröffentlicht werden
       (&ldquo;share-alike&rdquo;). Details zur Lizenz können Sie auf den Seiten
       der <Link
-<<<<<<< HEAD
-      href="https://www.opendatacommons.org/licenses/odbl/summary/index.html"
-      >Open Knowledge Foundation
-      </Link> nachlesen.
-    </Paragraph>
-    <Paragraph>Die Erklärung zu den Spalten in
-      dem JSON-Datensatz finden Sie in <Link
-        href="/uploads/kataster-ki/Spezifikation_Ausgabeformat_des_Strassenchecks.pdf"
-      >diesem PDF zu den Spezifikationen</Link>.
-    </Paragraph>
-    <Paragraph>
-      Unter folgendem Link gelangen Sie zu den <Link
-      href="https://raw.githubusercontent.com/FixMyBerlin/fixmy.data/master/SurveyResults_200414.json"
-    >Ergebnissen der Umfrage als JSON-Datensatz</Link>, die sie dort Einsehen und Herunterladen können.
-    </Paragraph>
-=======
         href="https://www.opendatacommons.org/licenses/odbl/summary/index.html"
       >Open Knowledge Foundation
       </Link> nachlesen. Die Erklärung zu den Spalten in
@@ -54,7 +35,6 @@ const SectionDataset = ({ toc }) => (
        (Format: JSON, Größe: ~41 MB,Format: ODbL)
      </DownloadInfo>
     </ButtonWrapper>
->>>>>>> origin/develop
   </>
 );
 
