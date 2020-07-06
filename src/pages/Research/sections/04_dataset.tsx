93689391d645cb5fe03a68ed3aa0dfa6c19dd980
s-pic <sascha.picard@gmx.de>
2020-07-06 08:44:42 +0200

    fix: deleted files are imported
    https://github.com/FixMyBerlin/fixmy.frontend/commit/15f0a27b2421fd406c2b99561a34268e1aeb137f
    introduced the problem

--- src/pages/Research/sections/04_dataset.tsx
+++ src/pages/Research/sections/04_dataset.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Heading, Paragraph } from '~/components2/Article';
 import Link from '~/components2/Link';
-import { AnchorButton } from '~/components2/Button';
-import ButtonWrapper from '../components/ButtonWrapper';
-import DownloadInfo from '~/pages/Research/components/DownloadInfo';
 
 const SectionDataset = ({ toc }) => (
   <>
@@ -15,26 +12,18 @@ const SectionDataset = ({ toc }) => (
       unter der Bedingung, dass abgeleitete Werke auch veröffentlicht werden
       (&ldquo;share-alike&rdquo;). Details zur Lizenz können Sie auf den Seiten
       der <Link
-        href="https://www.opendatacommons.org/licenses/odbl/summary/index.html"
-      >Open Knowledge Foundation
-      </Link> nachlesen. Die Erklärung zu den Spalten in
+      href="https://www.opendatacommons.org/licenses/odbl/summary/index.html"
+    >Open Knowledge Foundation
+    </Link> nachlesen. Die Erklärung zu den Spalten in
       dem JSON-Datensatz finden Sie in <Link
-        href="/uploads/kataster-ki/Spezifikation_Ausgabeformat_des_Strassenchecks.pdf"
-      >diesem PDF zu den Spezifikationen</Link>.
-      Laden Sie sich die Ergebnisse der Umfrage über den folgenden Button als
-      JSON-Datensatz herunter.
+      href="/uploads/kataster-ki/Spezifikation_Ausgabeformat_des_Strassenchecks.pdf"
+    >diesem PDF zu den Spezifikationen</Link>.
     </Paragraph>
-    <ButtonWrapper>
-      <AnchorButton
-        flat
-        href="https://raw.githubusercontent.com/FixMyBerlin/fixmy.data/master/SurveyResults_200414.json"
-        target="_blank"
-        rel="noopener noreferrer"
-      >Datensatz herunterladen</AnchorButton>
-     <DownloadInfo>
-       (Format: JSON, Größe: ~41 MB,Format: ODbL)
-     </DownloadInfo>
-    </ButtonWrapper>
+    <Paragraph>
+      Unter folgendem Link gelangen Sie zu den <Link
+      href="https://raw.githubusercontent.com/FixMyBerlin/fixmy.data/master/SurveyResults_200414.json"
+    >Ergebnissen der Umfrage als JSON-Datensatz</Link>, die sie dort Einsehen und Herunterladen können.
+    </Paragraph>
   </>
 );
 
