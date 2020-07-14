ae4d5b11dac1db805bdf96dbf2bded22627fe8e3
Vincent Ahrend <mail@vincentahrend.com>
2020-07-14 13:11:38 +0200

    Update article components
    - Add image role and alt prop
    - Move breakpoint to site-wide default
    - Adjust TOC style
    - Increase H1 font-size
    - Adjust h2 margins
    - Refactor quote component and add long variation

--- src/components2/Article/Typography/Quote.tsx
+++ src/components2/Article/Typography/Quote.tsx
@@ -1,20 +1,16 @@
+import React from 'react';
 import styled from 'styled-components';
 import { media } from '~/styles/utils';
 
 interface QuoteProps {
   sourceText?: string;
   long?: boolean;
+  children: React.ReactNode;
 }
 
-const Quote = styled.div<QuoteProps>`
-  background: white;
-  box-shadow: 2px 6px 60px 0 rgba(0, 0, 0, 0.09);
-  padding: 1.5em;
-  font-size: ${(props) => (props.long ? '1em' : '1.5em')};
-  font-style: italic;
-  font-weight: 600;
-  line-height: 1.5em;
-  border-radius: 6px;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   position: relative;
   max-width: 620px;
   margin: 1em auto 1.8em auto;
@@ -22,23 +18,41 @@ const Quote = styled.div<QuoteProps>`
   ${media.m`
     margin: 3em auto 3.8em auto;
   `}
-
-  &:after {
-    content: "${(p) => p.sourceText}";
-    display: ${(p) => (p.sourceText ? 'block' : 'none')};
-    position: absolute;
-    background: #cf0a7d;
-    font-weight: 700;
-    font-size: ${(props) => (props.long ? '0.87em' : '0.58em')};
-    font-style: normal;
-    padding: 11px 22px;
-    border-radius: 3px;
-    color: white;
-    bottom: 0;
-    top: auto;
-    left: 50%;
-    transform: translate(-50%, 50%);
-  }
 `;
+
+const QuoteStyle = styled.div`
+  background: white;
+  box-shadow: 2px 6px 60px 0 rgba(0, 0, 0, 0.09);
+  padding: ${(props: QuoteProps) =>
+    props.long && props.sourceText ? '2.5em' : '1.5em'};
+  font-size: ${(props: QuoteProps) => (props.long ? '1em' : '1.5em')};
+  font-style: italic;
+  font-weight: 600;
+  line-height: 1.5em;
+  border-radius: 6px;
+`;
+
+const Attribution = styled.div`
+  display: inline;
+  background: #cf0a7d;
+  font-weight: 700;
+  font-size: 0.875em;
+  font-style: normal;
+  line-height: 1.25;
+  padding: 11px 22px;
+  border-radius: 3px;
+  color: white;
+  align-self: center;
+  transform: translateY(-50%);
+`;
+
+const Quote = (props: QuoteProps) => (
+  <Wrapper>
+    <QuoteStyle {...props}>{props.children}</QuoteStyle>
+    {props.sourceText && (
+      <Attribution {...props}>{props.sourceText}</Attribution>
+    )}
+  </Wrapper>
+);
 
 export default Quote;
