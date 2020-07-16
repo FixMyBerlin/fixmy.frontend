48c87fad83d516e1a5ba67b1d0f5d5cb3db9b931
Vincent Ahrend <mail@vincentahrend.com>
2020-07-16 17:56:28 +0200

    Fix linter

--- src/apps/Gastro/components/RegistrationForm/index.tsx
+++ src/apps/Gastro/components/RegistrationForm/index.tsx
@@ -8,7 +8,6 @@ import styled from 'styled-components';
 import Button from '~/components2/Button';
 import { Form } from '~/components2/Form';
 import logger from '~/utils/logger';
-import config from '~/apps/Gastro/config';
 import { GastroRegistration } from '~/apps/Gastro/types';
 import api from '~/apps/Gastro/api';
 import { validate } from './validate';
