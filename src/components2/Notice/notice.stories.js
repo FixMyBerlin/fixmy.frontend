af04caa7eead7e6e1b5973d6df8090154eb0f19b
Vincent Ahrend <mail@vincentahrend.com>
2020-07-23 15:12:39 +0200

    Add story for notice component

---
+++ src/components2/Notice/notice.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { action } from '@storybook/addon-actions';
+import Notice, { ApiNotice as ApiNoticeComponent } from '.';
+
+export default {
+  title: 'Generic / Notice',
+  decorators: [(fn) => <Container maxWidth="md">{fn()}</Container>]
+};
+
+export const Regular = () => <Notice>Dies ist ein Hinweis</Notice>;
+
+export const ApiNotice = () => (
+  <ApiNoticeComponent>
+    Dies ist ein Hinweis für eine fehlgeschlagene API-Abfrage
+  </ApiNoticeComponent>
+);
+
+export const ApiNoticeRetry = () => (
+  <ApiNoticeComponent onRetry={action('clicked retry')}>
+    Dies ist ein Hinweis für eine fehlgeschlagene API-Abfrage
+  </ApiNoticeComponent>
+);
