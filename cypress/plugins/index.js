d020ac2e3f37093ab78dd1447a9ac8ec869fccdb
s-pic <sascha.picard@gmail.com>
2020-08-05 21:34:37 +0200

    chore(cypress config): refactor, doc and cleanup plugins declarations

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 require('dotenv').config();
 const wp = require('@cypress/webpack-preprocessor');
 
@@ -5,55 +6,6 @@ const log = require('debug')('cypress:plugins');
 
 const baseConfig = require('../../cypress.json');
 const webpackOptions = require('../../webpack/webpack.config.dev.js');
-
-// immediately open dev tools so we can inspect breakpoint halts
-// (when we added a "debugger" statement in our code
-const setAutoDevTools = (args) => args.push('--auto-open-devtools-for-tabs');
-
-// Set Chrome's window position and size
-//
-// Use an environment variable to control where the Chrome browser window will
-// be opened by Cypress. As an example:
-//
-//   CYPRESS_BROWSER_WINDOW="1920,1080;1920,0" npm run test:e2e-chrome
-//
-// would run cypress tests with a window of size 1920x1080 with its upper left
-// corner at 1920,0 on the monitor.
-const setWindowPos = (args) => {
-  const { cypressBrowserWindow } = process.env;
-  if (cypressBrowserWindow == null) return;
-
-  const [windowSize, windowPosition] = cypressBrowserWindow.split(';');
-  args.push(
-    '--user-data-dir="~/chrome-test-user"',
-    `--window-size=${windowSize}`,
-    `--window-position=${windowPosition}`
-  );
-};
-
-/**
- * Generate an array of pattern for selecting tests to run
- *
- * Expects an environment variable `REGION` with a corresponding entry
- * in `cypress.json` under the key `whitelist`, containing an array of
- * path segments to match for.
- *
- * @returns string[] A list of file patterns
- */
-const getPatternsForRegion = () => {
-  const makePattern = (page) => `**/${page}/**/*.e2e.test.js`;
-
-  const region = process.env.REGION;
-  const whitelist =
-    baseConfig.whitelist[region] || baseConfig.whitelist.default;
-  const patterns = whitelist.map(makePattern);
-
-  log(`Selecting tests for region ${region} using patterns ${patterns}`);
-  if (!Object.keys(baseConfig).includes(region))
-    log('No test whitelist defined for region, using default');
-
-  return patterns;
-};
 
 module.exports = (on, config) => {
   // only include certain tests
@@ -80,3 +32,62 @@ module.exports = (on, config) => {
     env: process.env
   };
 };
+
+/**
+ * Extend browser arguments to immediately open dev tools so we can inspect breakpoint halts
+ * (when we added a "debugger" statement in our code
+ * @param {string[]} args - Command-line args Passed when the browser is launched,
+ * see https://docs.cypress.io/api/plugins/browser-launch-api.html#Modify-browser-launch-arguments-preferences-and-extensions
+ */
+function setAutoDevTools(args) {
+  args.push('--auto-open-devtools-for-tabs');
+}
+
+/**
+ * Extend browser arguments to set Chrome's window position and size
+ * Use an environment variable to control where the Chrome browser window will
+ * be opened by Cypress. As an example:
+ *
+ * $ cross-env CYPRESS_BROWSER_WINDOW="1920,1080;1920,0" npm run test:e2e-chrome
+ *
+ * would run cypress tests with a window of size 1920x1080 with its upper left
+ * corner at 1920,0 on the monitor.
+ *
+ * @param {string[]} args - Command-line args Passed when the browser is launched,
+ * see https://docs.cypress.io/api/plugins/browser-launch-api.html#Modify-browser-launch-arguments-preferences-and-extensions
+ */
+function setWindowPos(args) {
+  const { cypressBrowserWindow } = process.env;
+  if (cypressBrowserWindow == null) return;
+
+  const [windowSize, windowPosition] = cypressBrowserWindow.split(';');
+  args.push(
+    '--user-data-dir="~/chrome-test-user"',
+    `--window-size=${windowSize}`,
+    `--window-position=${windowPosition}`
+  );
+}
+
+/**
+ * Generate an array of pattern for selecting tests to run
+ *
+ * Expects an environment variable `REGION` with a corresponding entry
+ * in `cypress.json` under the key `whitelist`, containing an array of
+ * path segments to match for.
+ *
+ * @returns string[] A list of file patterns
+ */
+function getPatternsForRegion() {
+  const makePattern = (page) => `**/${page}/**/*.e2e.test.js`;
+
+  const region = process.env.REGION;
+  const whitelist =
+    baseConfig.whitelist[region] || baseConfig.whitelist.default;
+  const patterns = whitelist.map(makePattern);
+
+  log(`Selecting tests for region ${region} using patterns ${patterns}`);
+  if (!Object.keys(baseConfig).includes(region))
+    log('No test whitelist defined for region, using default');
+
+  return patterns;
+}
