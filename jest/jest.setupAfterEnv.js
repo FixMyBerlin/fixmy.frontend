2c193e9542ed64d7e64f193a0a90bb4456dcfb71
Vincent Ahrend <mail@vincentahrend.com>
2020-08-19 10:37:24 +0200

    Github Actions CI (#514)

--- jest/jest.setupAfterEnv.js
+++ jest/jest.setupAfterEnv.js
@@ -8,6 +8,7 @@ import { setupServer } from 'msw/node';
 import handlers from './mocks/mswHandlers';
 
 // Setup requests interception using the given handlers.
+// eslint-disable-next-line import/prefer-default-export
 export const server = setupServer(...handlers);
 
 beforeAll(() => {
@@ -27,4 +28,3 @@ afterAll(() => {
 
 // increase timeout for async tests
 jest.setTimeout(30000);
-
--- jest/mocks/mswHandlers.js
+++ jest/mocks/mswHandlers.js
@@ -1,5 +1,5 @@
 import { rest } from 'msw';
-import mockLocationSuggestions from './mockLocationSuggestions.json'
+import mockLocationSuggestions from './mockLocationSuggestions.json';
 
 // msw route handlers designated to be used in various places,
 // see https://kentcdodds.com/blog/stop-mocking-fetch
@@ -7,9 +7,7 @@ export default [
   rest.get(
     new RegExp('^https://api.mapbox.com/geocoding/v5/mapbox.places'),
     (req, res, ctx) => {
-      return res(
-        ctx.json(mockLocationSuggestions)
-      );
+      return res(ctx.json(mockLocationSuggestions));
     }
   )
-]
+];
