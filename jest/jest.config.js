848ac4e0ffe9a125e3f596ce14ff051d6e8f37d2
Vincent Ahrend <mail@vincentahrend.com>
2020-09-03 16:51:29 +0200

    Update coverage to ignore test and stories modules

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -30,6 +30,8 @@ module.exports = {
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{js,ts,tsx}'],
+  coveragePathIgnorePatterns: ['node_modules', 'stories.', '.test.'],
 
   // An array of directory names to be searched recursively up from the
   // requiring module's location
