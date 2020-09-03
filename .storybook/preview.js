acf871557d304c5a601f5c0f09a632e9ab4a94c6
Vincent Ahrend <mail@vincentahrend.com>
2020-09-03 12:44:55 +0200

    Add storybook decorators for intl and router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { configure, addDecorator } from '@storybook/react';
 import { ThemeProvider } from '@material-ui/styles';
 import Store from '~/store';
 import { getTheme } from '~/styles/mui-utils';
+import { IntlProvider } from 'react-intl';
+
+import messages from '~/lang/compiled/de.json';
 
 const StoreDecorator = (storyFn) => (
   <Provider store={Store}>{storyFn()}</Provider>
@@ -17,3 +22,20 @@ const ThemeDecorator = (storyFn) => {
 };
 
 addDecorator(ThemeDecorator);
+
+const IntlDecorator = (storyFn) => {
+  return (
+    <IntlProvider locale="de" defaultLocale="de" messages={messages}>
+      {storyFn()}
+    </IntlProvider>
+  );
+};
+
+addDecorator(IntlDecorator);
+
+const RouterDecorator = (storyFn) => {
+  const history = createMemoryHistory();
+  return <Router history={history}>{storyFn()}</Router>;
+};
+
+addDecorator(RouterDecorator);
