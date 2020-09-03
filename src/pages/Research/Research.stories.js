45b32396b629279d793128be29219b912de2236c
Vincent Ahrend <mail@vincentahrend.com>
2020-09-03 16:22:12 +0200

    Add BarChart stories

--- src/pages/Research/Research.stories.js
+++ src/pages/Research/Research.stories.js
@@ -4,7 +4,11 @@ import { ArticleWrapper } from '~/components2/Article/';
 import BarChartComponent from './components/BarChart';
 
 export default {
-  title: 'Research',
+  title: 'Research / BarChart',
+  component: BarChartComponent.Wrapper,
+  subcomponents: {
+    BarChart: BarChartComponent
+  },
   decorators: [
     (storyFn) => (
       <ArticleWrapper>
@@ -14,20 +18,45 @@ export default {
   ]
 };
 
-export const BarChart = () => (
-  <BarChartComponent.Wrapper
-    title="RVA Führung Fahrbahn und Seitenraum"
-    source="Anm: Durchschnitt der Bewertungen aller Situationen mit RVA."
-  >
-    <BarChartComponent
-      title="RVA auf Fahrbahn"
-      data={[74.4550633, 21.22332867, 39.02780333, 32.303362]}
-      feelsafe={71.331}
-    />
-    <BarChartComponent
-      title="RVA im Seitenraum"
-      data={[34.3291067, 14.276962, 34.29517267, 47.99495467]}
-      feelsafe={82.29}
-    />
+const Template = ({ title, source, barcharts }) => (
+  <BarChartComponent.Wrapper title={title} source={source}>
+    {barcharts.map((args) => (
+      <BarChartComponent {...args} />
+    ))}
   </BarChartComponent.Wrapper>
 );
+
+export const UserGroups = Template.bind({});
+
+UserGroups.args = {
+  title: 'RVA Führung Fahrbahn und Seitenraum',
+  source: 'Anm: Durchschnitt der Bewertungen aller Situationen mit RVA.',
+  barcharts: [
+    {
+      title: 'RVA auf Fahrbahn',
+      data: [74.4550633, 21.22332867, 39.02780333, 32.303362],
+      feelsafe: 71.331
+    },
+    {
+      title: 'RVA im Seitenraum',
+      data: [34.3291067, 14.276962, 34.29517267, 47.99495467],
+      feelsafe: 82.29
+    }
+  ]
+};
+
+export const Weights = Template.bind({});
+
+Weights.args = {
+  ...UserGroups.args,
+  barcharts: [
+    {
+      title: 'RVA auf Fahrbahn',
+      data: [74.4550633]
+    },
+    {
+      title: 'RVA im Seitenraum',
+      data: [34.3291067]
+    }
+  ]
+};
