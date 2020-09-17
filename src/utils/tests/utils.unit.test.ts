9b714b07cc076710ece5829bf8b75869476c234d
Vincent Ahrend <mail@vincentahrend.com>
2020-09-17 12:49:17 +0200

    Fix sorting util (#538)
    * Shorten project list fixture used in unit test
    
    * Replace expected values with handcrafted examples
    
    * Let codecov be more lenient in failing tests
    
    * Remove unused import

--- src/utils/tests/utils.unit.test.ts
+++ src/utils/tests/utils.unit.test.ts
@@ -1,35 +1,26 @@
 import { sortByKey } from '../utils';
 import projectList from './projectList.json';
-import projectListSorted from './projectListSorted';
 
 describe('sortByKey', () => {
   it('sorts lists of objects with a default sort', () => {
     const sortOutput = projectList.sort(sortByKey());
-    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual(
-      JSON.stringify(projectListSorted.byId)
-    );
+    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual('[4,6,7]');
   });
 
   it('sorts lists of objects by id', () => {
     const sortOutput = projectList.sort(sortByKey('id'));
-    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual(
-      JSON.stringify(projectListSorted.byId)
-    );
+    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual('[4,6,7]');
   });
 
   it('sorts lists of objects by likes', () => {
     const sortOutput = projectList.sort(sortByKey('likes'));
-    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).not.toEqual(
-      JSON.stringify(projectListSorted.byLikes)
-    );
+    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual('[4,7,6]');
   });
 
   it('sorts lists of objects by construction date', () => {
     const sortOutput = projectList.sort(
       sortByKey('construction_completed_date')
     );
-    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).not.toEqual(
-      JSON.stringify(projectListSorted.byDate)
-    );
+    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual('[7,6,4]');
   });
 });
