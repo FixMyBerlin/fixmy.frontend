3190c1be6522cd59484bb2562f3953d30b997303
Vincent Ahrend <mail@vincentahrend.com>
2020-09-17 12:13:24 +0200

    Replace expected values with handcrafted examples

--- src/utils/tests/utils.unit.test.ts
+++ src/utils/tests/utils.unit.test.ts
@@ -12,24 +12,18 @@ describe('sortByKey', () => {
 
   it('sorts lists of objects by id', () => {
     const sortOutput = projectList.sort(sortByKey('id'));
-    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual(
-      JSON.stringify(projectListSorted.byId)
-    );
+    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual('[4,6,7]');
   });
 
   it('sorts lists of objects by likes', () => {
     const sortOutput = projectList.sort(sortByKey('likes'));
-    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).not.toEqual(
-      JSON.stringify(projectListSorted.byLikes)
-    );
+    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual('[4,7,6]');
   });
 
   it('sorts lists of objects by construction date', () => {
     const sortOutput = projectList.sort(
       sortByKey('construction_completed_date')
     );
-    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).not.toEqual(
-      JSON.stringify(projectListSorted.byDate)
-    );
+    expect(JSON.stringify(sortOutput.map((obj) => obj.id))).toEqual('[7,6,4]');
   });
 });
