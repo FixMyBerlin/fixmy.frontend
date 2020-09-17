e46e5ee518548c52e61295de429616cab6690ee6
Vincent Ahrend <mail@vincentahrend.com>
2020-09-17 17:18:58 +0200

    Fix region env var for Cypress runner

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,17 @@
+import debug from 'debug';
 import { RootConfig, Region } from '~/types';
 import defaultConfig from './default';
 import berlin from './berlin';
 import aachen from './aachen';
 import eichwalde from './eichwalde';
 
-const region = (process.env.REGION as Region) || 'berlin';
+const log = debug('fmc:config');
+
+let region = (process.env.REGION as Region) || 'berlin';
+
+if (window.Cypress) {
+  region = window.Cypress.env('REGION');
+}
 
 const AVAILABLE_REGIONS = {
   berlin,
@@ -17,7 +24,7 @@ const instanceConfig = AVAILABLE_REGIONS[region];
 if (Object.keys(AVAILABLE_REGIONS).indexOf(region) === -1) {
   // Need to use console log to avoid circular import in logger module
   // eslint-disable-next-line no-console
-  console.error('No region defined for this instance');
+  log('No region defined for this instance');
 }
 
 const rootConfig: RootConfig = {
