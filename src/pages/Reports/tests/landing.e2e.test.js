cccd59219c7dc16a8ba94f6d4674174fee810831
s-pic <sascha.picard@gmail.com>
2020-09-20 20:27:24 +0200

    fix(landing.e2e.test.js): failing tests

--- src/pages/Reports/tests/landing.e2e.test.js
+++ src/pages/Reports/tests/landing.e2e.test.js
@@ -2,55 +2,62 @@ import { cyElem } from '~/../cypress/support/utils';
 import config from '~/pages/Reports/config';
 
 describe('The reports landing page', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit(config.routes.reports.landing);
-    if (config.reports.landing?.logo) {
-      cyElem('reports-landing-logo').should((img) => {
-        expect(img[0].naturalWidth).to.be.greaterThan(0);
+  });
+
+  it(
+    'A user can find answers to common questions ' +
+      'by revealing the answer with a click on the question',
+    () => {
+      // initially, answers should be hidden
+      cyElem('reports-landing-faq-item').within(() => {
+        cy.get('.Collapsible__contentInner').should('not.be.visible');
       });
+      // when a question is clicked, the answer is revealed
+      cyElem('reports-landing-faq-item')
+        .should('be.visible')
+        .first()
+        .scrollIntoView()
+        .click();
+      cyElem('reports-landing-faq-item').within(() => {
+        cy.get('.Collapsible__contentInner').should('be.visible');
+      });
+      // an item collapses once it is clicked again
+      cyElem('reports-landing-faq-item')
+        .first()
+        .click();
+      cyElem('reports-landing-faq-contents').should('not.be.visible');
     }
-  });
-  it('has a title', () => {
-    cyElem('reports-landing-header').should('be.visible');
-    cyElem('reports-landing-header')
+  );
+
+  it('A user can find links to see all existing reports', () => {
+    cyElem('reports-landing-mapLink')
+      .should('be.visible')
       .first()
-      .contains(config.reports.region);
+      .click();
+
+    // the url should be "speaking"
+    cy.url().should('include', 'karte');
+    // there should be a mal
+    cyElem('reports-basemap').should('be.visible');
   });
-  // it('has a button that takes users to the report form', () => {
-  //   cyElem('reports-landing-cta')
-  //     .should('be.visible')
-  //     .first()
-  //     .click()
-  //     .then(() => cy.url().should('be', '**/neu/1'));
-  //   cy.visit(config.routes.reports.landing);
-  // });
-  // it('takes users to the map of existing reports when clicked', () => {
-  //   cyElem('reports-landing-mapLink')
-  //     .should('be.visible')
-  //     .first()
-  //     .click()
-  //     .then(() => cy.url().should('be', '**/karte'));
-  //   cy.visit(config.routes.reports.landing);
-  // });
-  // describe('an FAQ item', () => {
-  //   it('extends when clicked', () => {
-  //     cyElem('reports-landing-faq-item').within(() => {
-  //       cy.get('.Collapsible__contentInner').should('not.be.visible');
-  //     });
-  //     cyElem('reports-landing-faq-item')
-  //       .should('be.visible')
-  //       .first()
-  //       .scrollIntoView()
-  //       .click();
-  //     cyElem('reports-landing-faq-item').within(() => {
-  //       cy.get('.Collapsible__contentInner').should('be.visible');
-  //     });
-  //   });
-  //   it('collapses when clicked again', () => {
-  //     cyElem('reports-landing-faq-item')
-  //       .first()
-  //       .click();
-  //     cyElem('reports-landing-faq-contents').should('not.be.visible');
-  //   });
-  // });
+
+  it('A user can find links to see create a new report', () => {
+    if (!config.reports.enabled) {
+      cy.log(
+        'skipping the test that ensures tge presence of an element navigation the user' +
+          ' to the new report form The feature is disabled via config. ' +
+          'Please make sure this is happening intentionally.'
+      );
+      return;
+    } // anyways go in with the test
+
+    cyElem('reports-landing-cta')
+      .should('be.visible')
+      .first()
+      .click();
+
+    cy.url().should('include', 'neu');
+  });
 });
