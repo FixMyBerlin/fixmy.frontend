bb9a80fa60b13b4956836ea1769f292eb0ef58d8
Vincent Ahrend <mail@vincentahrend.com>
2020-09-29 14:43:21 +0200

    Add detail panel for linked report entries (#579)

--- src/pages/Reports/pages/OverviewMap/components/ReportDetails/Body.js
+++ src/pages/Reports/pages/OverviewMap/components/ReportDetails/Body.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Link from '~/components/Link';
 import SubHeading from '~/pages/Reports/pages/SubmitReport/components/SubHeading';
 // import HorizontalRuler from '~/pages/Reports/pages/SubmitReport/components/HorizontalRuler';
 import config from '~/pages/Reports/config';
+import { getReportStatusCaption } from '~/pages/Reports/apiservice';
 
 const IndicatorSection = styled.div`
   display: flex;
@@ -36,9 +38,19 @@ const IndicatorValue = styled(Text)`
 //   });
 // }
 
+const EntryLink = styled(Link)`
+  && {
+    color: ${config.colors.black};
+    border-bottom: 1px solid ${config.colors.interaction};
+    line-height: 1.6;
+  }
+`;
+
 const DetailsBody = ({
   description,
-  details: { fee_acceptable: isFeeAcceptable }
+  details: { fee_acceptable: isFeeAcceptable },
+  origin,
+  plannings
   // created_date: createdDate
 }) => (
   <>
@@ -48,12 +60,45 @@ const DetailsBody = ({
         <Text data-cy="reports-detail-description">{description}</Text>
       </>
     )}
-
     <IndicatorSection>
       <IndicatorTitle>Bedarf Fahrradparkhaus:</IndicatorTitle>
       <IndicatorValue>{isFeeAcceptable ? 'ja' : 'nein'}</IndicatorValue>
     </IndicatorSection>
 
+    {origin.length > 0 && (
+      <>
+        <SubHeading alignLeft>Eingeflossene Meldungen</SubHeading>
+        <p>Folgende Meldungen sind in dieser Planung berücksichtigt worden:</p>
+        <ul>
+          {origin.map((source) => (
+            <li key={`origin-${source.id}`}>
+              <EntryLink to={`${config.routes.reports.map}/${source.id}`}>
+                {source.address}
+              </EntryLink>
+            </li>
+          ))}
+        </ul>
+      </>
+    )}
+
+    {plannings.length > 0 && (
+      <>
+        <SubHeading alignLeft>Umsetzung in der Nähe</SubHeading>
+        <p>
+          In der unmittelbaren Nähe dieses Vorschlags werden Radbügel an
+          folgenden Standorten umgesetzt:
+        </p>
+        <ul>
+          {plannings.map((source) => (
+            <li key={`planning-${source.id}`}>
+              <EntryLink to={`${config.routes.reports.map}/${source.id}`}>
+                {source.address} ({getReportStatusCaption(source.status)})
+              </EntryLink>
+            </li>
+          ))}
+        </ul>
+      </>
+    )}
     {/*
     <HorizontalRuler className="light" />
 
