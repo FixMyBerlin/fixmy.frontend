06a4bb882dbcdf3eea7318c1297b7832fb6ba303
Vincent Ahrend <mail@vincentahrend.com>
2020-12-03 16:04:27 +0100

    Add pin entries to legend

--- src/pages/Reports/pages/OverviewMap/components/MapLegend/LegendLarge.tsx
+++ src/pages/Reports/pages/OverviewMap/components/MapLegend/LegendLarge.tsx
@@ -1,19 +1,79 @@
 import React from 'react';
 import styled from 'styled-components';
-import { LinkStyle } from '~/components2/Link';
+import config from '~/pages/Reports/config';
 import { BaseWrapper } from './LegendSmall';
+import ReportPin from '~/pages/Reports/components/ReportPin';
 
-const WrapperLarge = styled(BaseWrapper)`
+const Wrapper = styled(BaseWrapper)`
   display: flex;
-  height: 50%;
+  flex-direction: column;
+  padding: 10px 20px;
 `;
 
-const LinkButton = LinkStyle('button');
+const LegendWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 20px;
+`;
+
+const LegendCol = styled.div`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  margin-right: 10px;
+`;
+
+const LegendItem = styled.div`
+  color: ${config.colors.lightgrey};
+  display: flex;
+  flex-direction: row;
+  font-size: 0.9em;
+  line-height: 1.16;
+  margin-bottom: 10px;
+  & img {
+    margin-right: 10px;
+  }
+`;
 
 const LegendLarge = ({ onToggle }) => (
-  <WrapperLarge>
-    <LinkButton onClick={onToggle}>Weniger Details</LinkButton>
-  </WrapperLarge>
+  <Wrapper>
+    <h2>Legende</h2>
+    <LegendWrapper>
+      <LegendCol>
+        <LegendItem>
+          <ReportPin status="report_verification" />
+          Meldung in Prüfung
+        </LegendItem>
+        <LegendItem>
+          <ReportPin status="report_accepted" />
+          Meldung wird nahebei umgesetzt
+        </LegendItem>
+        <LegendItem>
+          <ReportPin status="report_rejected" />
+          Meldung abgelehnt
+        </LegendItem>
+        <LegendItem>
+          <ReportPin status="report_inactive" />
+          Meldung inaktiv
+        </LegendItem>
+      </LegendCol>
+      <LegendCol>
+        <LegendItem>
+          <ReportPin status="report_new" /> Neue Meldung
+        </LegendItem>
+        <LegendItem>
+          <ReportPin status="planning" /> In Planung
+        </LegendItem>
+        <LegendItem>
+          <ReportPin status="execution" /> In Umsetzung
+        </LegendItem>
+        <LegendItem>
+          <ReportPin status="done" /> Fertiggestellt
+        </LegendItem>
+      </LegendCol>
+    </LegendWrapper>
+  </Wrapper>
 );
 
 export default LegendLarge;
--- src/pages/Reports/pages/OverviewMap/components/MapLegend/LegendSmall.tsx
+++ src/pages/Reports/pages/OverviewMap/components/MapLegend/LegendSmall.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import { LinkStyle } from '~/components2/Link';
+import config from '~/pages/Reports/config';
 
 export const BaseWrapper = styled.div`
   position: relative;
   width: 100%;
   bottom: 0;
   display: flex;
+  background-color: ${config.colors.darkbg};
+  color: ${config.colors.white};
 `;
 
 const WrapperSmall = styled(BaseWrapper)`
   display: flex;
 `;
 
-const LinkButton = LinkStyle('button');
+const LinkButton = styled(LinkStyle('button'))`
+  color: ${config.colors.white};
+`;
 
 const LegendSmall = ({ onToggle }) => (
   <WrapperSmall>
