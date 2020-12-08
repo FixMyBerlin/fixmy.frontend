90ee8ce5c7282fc67b36f3ff7778552d0fbca4d6
Vincent Ahrend <mail@vincentahrend.com>
2020-12-08 13:42:32 +0100

    Add mapbox-gl mock

---
+++ jest/mocks/mapbox-gl.js
@@ -0,0 +1,15 @@
+// WebGL and other features used by MapboxGL are not available in the test
+// environment so mapbox-gl is mocked
+//
+// https://github.com/mapbox/mapbox-gl-js/issues/3436#issuecomment-485535598
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  GeolocateControl: jest.fn(),
+  Map: jest.fn(() => ({
+    addControl: jest.fn(),
+    on: jest.fn(),
+    remove: jest.fn(),
+  })),
+  NavigationControl: jest.fn(),
+}));
+
+export default undefined;
