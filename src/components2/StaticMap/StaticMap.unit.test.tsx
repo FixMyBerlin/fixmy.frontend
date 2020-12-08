41c3a635093fe221b277a990f9bff9d87b4786c0
Vincent Ahrend <mail@vincentahrend.com>
2020-12-08 15:39:13 +0100

    Add static map tests

--- src/components2/StaticMap/StaticMap.unit.test.tsx
+++ src/components2/StaticMap/StaticMap.unit.test.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import { screen } from '@testing-library/dom';
 import { render } from '~/utils/test-utils';
-import Map from '.';
+import StaticMap from '.';
 
-describe('<Map />', () => {
+const TEST_STYLE_URL = 'mapbox://styles/hejco/ck85ospzd0cre1ioa8d6gfuv9';
+const TEST_LOCATION = [13.415669, 52.513219];
+
+describe('<StaticMap />', () => {
   it('renders', () => {
-    const mockHandleInit = jest.fn();
     const { container } = render(
-      <Map onInit={mockHandleInit} className="testClassName" />
+      <StaticMap
+        className="testClassName"
+        mapboxStyle={TEST_STYLE_URL}
+        location={TEST_LOCATION as mapboxgl.LngLatLike}
+      />
     );
     expect(mapboxgl.Map).toBeCalled();
     expect(screen.getByLabelText('Interactive WebGL map')).toBeInTheDocument();
