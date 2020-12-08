77303a014e8ff1891061c0521a02d07bf4dac14f
Vincent Ahrend <mail@vincentahrend.com>
2020-12-08 18:30:12 +0100

    Add map

--- src/components2/Article/Map/Map.tsx
+++ src/components2/Article/Map/Map.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import BaseMap from '~/components2/Map';
+import { media } from '~/styles/utils';
 
 const StyledMap = styled(BaseMap)`
   width: 100%;
   height: 35em;
 `;
 
-// eslint-disable-next-line react/style-prop-object
-const Map = ({ ...mapProps }) => <StyledMap {...mapProps} />;
+const Wrapper = styled.div`
+  margin: 0 -16px;
+  position: relative;
+
+  ${media.m`
+    margin: 3em auto;
+  `}
+`;
+
+const Map = ({ ...mapProps }) => (
+  <Wrapper>
+    <StyledMap {...mapProps} />
+  </Wrapper>
+);
 
 export default Map;
--- src/pages/ZESPlus-Research/sections/03_safety.tsx
+++ src/pages/ZESPlus-Research/sections/03_safety.tsx
@@ -1,4 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import {
+  Heading,
+  SectionProps,
+  Map as ArticleMap,
+} from '~/components2/Article';
+import { media } from '~/styles/utils';
+import config from '~/config';
+
+const MAP_STYLE_SAFETY = 'mapbox://styles/hejco/ckguzkrtq06em19l9477wwzc2';
+// const MAP_STYLE_QUALITY = 'mapbox://styles/hejco/ckhurerfv3wgv1aog7t7ek3gs';
+// const MAP_STYLE_TARGET = 'mapbox://styles/hejco/ckhufbloj0gmz19pk0m6bd4d2';
 
 import { Heading, SectionProps } from '~/components2/Article';
 
@@ -7,6 +20,7 @@ const SectionSafety = (props: SectionProps) => (
     <Heading as="h2" {...props}>
       Auswertung der Sicherheit
     </Heading>
+    <ArticleMap mapboxStyle={MAP_STYLE_SAFETY} />
   </>
 );
 
