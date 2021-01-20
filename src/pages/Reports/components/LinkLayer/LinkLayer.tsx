da417c5a4f73531ca6cc8542c3fc395131ba8dfc
Vincent Ahrend <mail@vincentahrend.com>
2021-01-20 12:12:11 +0100

    Simplify LinkLayer component
    - INITIAL_DECK_VIEW_STATE removed because there were hard coded values
    and we can get the accurate view state from Mapbox anyway once the map is loaded
    for the first time
    - arc layer implemented without JSX, which was misleading (arc layer is not  a react
    component)
    - added comments
    - fixed resize event handler
    - removed unused canvas refs

--- src/pages/Reports/components/LinkLayer/LinkLayer.tsx
+++ src/pages/Reports/components/LinkLayer/LinkLayer.tsx
@@ -1,92 +1,72 @@
 import { ArcLayer } from '@deck.gl/layers';
-// ESlint complains because deck.gl doesn't provide this export but it's
-// available as a type export from @danmarshall/deckgl-typings
-// eslint-disable import/no-unresolved
-// @ts-ignore
-import type { ArcLayerProps } from '@deck.gl/layers';
 import DeckGL from '@deck.gl/react';
 import MapboxGL from 'mapbox-gl';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
-import config from '~/pages/Reports/config';
 import { selectors as mapStateSelectors } from '~/pages/Reports/state/OverviewMapState';
 
-import {
-  Arc,
-  compileTooltip,
-} from '~/pages/Reports/pages/OverviewMap/service/arcService';
+import { compileTooltip } from '~/pages/Reports/pages/OverviewMap/service/arcService';
 import { useTypedSelector } from '~/store';
-
-const configuredBounds = config.reports.overviewMap
-  .bounds as MapboxGL.LngLatBoundsLike;
-
-// The DeckGL component does not take bounds as prop, instead it takes longitude
-// and latitude. We calculate those from the given bounds.
-// TODO: factor out and test
-const [[minLng, minLat], [maxLng, maxLat]] = configuredBounds as number[][];
-const centerLng = minLng + (maxLng - minLng) / 2;
-const centerLat = minLat + (maxLat - minLat) / 2;
-
-// Viewport settings
-const INITIAL_DECK_VIEW_STATE = {
-  longitude: centerLng,
-  latitude: centerLat,
-  zoom: 17,
-  pitch: 0,
-  bearing: 0,
-};
 
 const LinkLayer = ({ children }) => {
   const [glContext, setGLContext] = useState<WebGLRenderingContext>();
   // couple deck gl and mapbox gl views
-  const [mapViewState, setMapViewState] = useState(INITIAL_DECK_VIEW_STATE);
-  const arcLayerProps: ArcLayerProps<Arc[]> = useTypedSelector((state) =>
+  const [mapViewState, setMapViewState] = useState<{}>();
+
+  // Construct arc layer from redux state
+  const arcLayerProps = useTypedSelector((state) =>
     mapStateSelectors.selectArcLayerProps(state.ReportsState.OverviewMapState)
   );
+  const layers = [new ArcLayer(arcLayerProps)];
 
-  const deckRef = useRef(null);
-  const mapRef = useRef<MapboxGL.Map>(null);
-
-  const synchronizeViewStates = useCallback((mapboxMap) => {
+  /**
+   * Transfer MapboxGL viewstate to Deck.gl to keep both views in sync
+   */
+  const applyMapboxViewState = useCallback((mapboxMap: MapboxGL.Map) => {
     const center = mapboxMap.getCenter();
-    setMapViewState({
-      ...mapViewState,
+    setMapViewState((prevState) => ({
+      ...prevState,
       zoom: mapboxMap.getZoom(),
       pitch: mapboxMap.getPitch(),
       bearing: mapboxMap.getBearing(),
       latitude: center.lat,
       longitude: center.lng,
-    });
+    }));
   }, []);
 
-  const handleInit = (map: MapboxGL.Map) => {
-    mapRef.current = map;
-    map.on('move', ({ target }) => synchronizeViewStates(target));
-    map.on('resize', synchronizeViewStates);
+  /**
+   * Attach event listeners to Mapbox react that allow synching Deck.gl viewstate
+   *
+   * Children event listeners are also called.
+   *
+   * @param map MapboxGL instance
+   */
+  const onMapLoad = (map: MapboxGL.Map) => {
+    applyMapboxViewState(map);
+    map.on('move', (args) => {
+      applyMapboxViewState(args.target);
+      if (children.props.onMove) children.props.onMove(args);
+    });
+    map.on('resize', ({ target }) => applyMapboxViewState(target));
     children.props.onLoad(map);
   };
 
+  // The child <BaseMap /> is displayed once Deck.gl's gl context is available
   const wrappedMap =
     glContext == null
       ? null
-      : React.cloneElement(children, { onLoad: handleInit });
+      : React.cloneElement(children, { onLoad: onMapLoad });
 
   return (
     <DeckGL
-      initialViewState={INITIAL_DECK_VIEW_STATE}
-      viewState={mapViewState}
-      ref={deckRef}
       controller
-      onWebGLInitialized={setGLContext}
       getTooltip={compileTooltip}
-      pickingRadius={8}
       id="reports-deckgl-canvas"
+      layers={layers}
+      onWebGLInitialized={setGLContext}
+      pickingRadius={8}
+      viewState={mapViewState}
     >
-      {/* ArcLayer can be used as a React component even though it actually
-    is none. May want to refactor this eventually because it's too much
-    magic for my taste https://deck.gl/docs/api-reference/react/deckgl#jsx-layers
-    // @ts-ignore */}
-      {arcLayerProps && <ArcLayer {...arcLayerProps} />}
       {wrappedMap}
     </DeckGL>
   );
