916ad59a8bcdb02d1013ec69957375920a558d6b
Vincent Ahrend <mail@vincentahrend.com>
2021-01-27 16:33:45 +0100

    Fix second test and cleanup test module

--- src/apps/Map/tests/MapState.unit.test.ts
+++ src/apps/Map/tests/MapState.unit.test.ts
@@ -1,20 +1,18 @@
+import { AnyAction } from 'redux';
 import configureMockStore from 'redux-mock-store';
 import thunk, { ThunkDispatch } from 'redux-thunk';
 import { rest } from 'msw';
+
+import { mswServer } from '~/../jest/msw/mswServer';
 import {
-  LoadPlanningData,
   loadPlanningData,
-  MapState,
   setPlanningData,
   setPlanningDataFetchState,
   SET_PLANNING_DATA,
 } from '~/apps/Map/MapState';
 import config from '~/config';
-import { mswServer } from '../../../../jest/msw/mswServer';
 import { RootState } from '~/store';
 import planningsResponseFixture from './fixtures/planningsResponse.json';
-import { AnyAction } from 'redux';
-import logger from '~/utils/logger';
 
 describe('Plannings Map Unit tests', () => {
   describe('loadPlanningData() thunk', () => {
@@ -22,26 +20,17 @@ describe('Plannings Map Unit tests', () => {
     const mockStore = configureMockStore<RootState, DispatchExts>([thunk]);
 
     it('does not dispatch any actions if plannings data has been fetched already', async () => {
-      /* ARRANGE: mock store with initial state */
-      const preSeededMapState = {
-        planningData: true,
-      } as MapState; // omitting non-relevant props
-      const preSeededRootState = {
-        MapState: preSeededMapState,
-      } as RootState;
-      const store = mockStore(preSeededRootState);
-
-      /* ACT: invoke loadPlanningsThunk */
-      const loadPlanningsThunk = loadPlanningData();
-      // @ts-ignore FIXME: properly type this
-      await store.dispatch(loadPlanningsThunk);
-
-      /* ASSERT: no action should have been called */
+      const store = mockStore({
+        // @ts-expect-error this is not a complete map state, but it should be enough
+        MapState: {
+          planningDataFetchState: 'success',
+        },
+      });
+      await store.dispatch(loadPlanningData());
       expect(store.getActions()).toHaveLength(0);
     });
 
     it(`dispatches ${SET_PLANNING_DATA} once the plannings JSON has been fetched successfully`, async () => {
-      /* ARRANGE: mock store, intercept request and mock response */
       const store = mockStore({
         // @ts-expect-error this is not a complete map state
         MapState: {
@@ -49,20 +38,17 @@ describe('Plannings Map Unit tests', () => {
         },
       });
 
-      const response = planningsResponseFixture;
       mswServer.use(
         rest.get(`${config.apiUrl}/projects`, (_, res, ctx) =>
-          res(ctx.json(response))
+          res(ctx.json(planningsResponseFixture))
         )
       );
 
-      /* ACT: invoke thunk */
       await store.dispatch(loadPlanningData());
 
-      /* ASSERT: action containing the api response has been dispatched */
       const expectedActions: AnyAction[] = [
         setPlanningDataFetchState('pending'),
-        setPlanningData(response),
+        setPlanningData(planningsResponseFixture),
         setPlanningDataFetchState('success'),
       ];
       expect(store.getActions()).toEqual(expectedActions);
