e9a761b35bba939b5b8bdc8d145597a809cba671
Vincent Ahrend <mail@vincentahrend.com>
2021-04-13 18:03:55 +0200

    Add time section

---
+++ src/apps/Gastro/components/EventForm/SectionTime.tsx
@@ -0,0 +1,72 @@
+import { isBefore } from 'date-fns';
+import { Field } from 'formik';
+import { DatePicker, TimePicker } from 'formik-material-ui-pickers';
+import React from 'react';
+import styled from 'styled-components';
+
+const InvisiLabel = styled.label`
+  display: none;
+`;
+
+const timePickerOpts = {
+  ampm: false,
+  minutesStep: 5,
+  views: ['hours', 'minutes'],
+};
+
+const SectionUsage = ({ values, handleChange, minDate }) => (
+  <section>
+    <h3>Wann soll die Veranstaltung stattfinden</h3>
+    <p>
+      Hinweis: Sondergenehmigungen sind von Montag bis Samstag in den Zeiten 6
+      bis 22 Uhr möglich (Sonntag und Feiertags nur in Ausnahmefällen). Bitte
+      beachten Sie, dass nur eine bestimmte Anzahl von Veranstaltungen pro Monat
+      erlaubt werden kann. Über die Belegung der Flächen entscheidet das
+      Bezirksamt im Einzelfall. Anträge müssen mindestens 20 Werktage vor
+      Veranstaltungsbeginn gestellt werden.
+    </p>
+    <InvisiLabel htmlFor="date">Datum</InvisiLabel>
+    <Field
+      component={DatePicker}
+      fullWidth
+      name="date"
+      label="Datum"
+      format="PP"
+      shouldDisableDate={(dt: Date) => isBefore(dt, minDate)}
+    />
+    <InvisiLabel htmlFor="setup_start">Beginn des Aufbaus</InvisiLabel>
+    <Field
+      component={TimePicker}
+      fullWidth
+      name="setup_start"
+      label="Beginn des Aufbaus"
+      {...timePickerOpts}
+    />
+    <InvisiLabel htmlFor="event_start">Beginn der Veranstaltung</InvisiLabel>
+    <Field
+      component={TimePicker}
+      fullWidth
+      name="event_start"
+      label="Beginn der Veranstaltung"
+      {...timePickerOpts}
+    />
+    <InvisiLabel htmlFor="event_end">Ende der Veranstaltung</InvisiLabel>
+    <Field
+      component={TimePicker}
+      fullWidth
+      name="event_end"
+      label="Ende der Veranstaltung"
+      {...timePickerOpts}
+    />
+    <InvisiLabel htmlFor="teardown_end">Ende des Abbaus</InvisiLabel>
+    <Field
+      component={TimePicker}
+      fullWidth
+      name="teardown_end"
+      label="Ende des Abbaus"
+      {...timePickerOpts}
+    />
+  </section>
+);
+
+export default SectionUsage;
--- src/apps/Gastro/components/EventForm/validate.ts
+++ src/apps/Gastro/components/EventForm/validate.ts
@@ -1,14 +1,13 @@
-/* eslint-disable camelcase */
-
+import { isAfter, isBefore, setHours } from 'date-fns';
 import debug from 'debug';
 
-import { EventApplication } from '../../types';
+import { EventApplication, PermitApplication } from '../../types';
 
 const logger = debug('fmc:Gastro:Registration');
 
 type ValidateErrors = Partial<EventApplication>;
 
-const validate = (values: EventApplication) => {
+const validate = (values: any) => {
   const errors: ValidateErrors = {};
 
   if (!values.first_name) {
@@ -25,6 +24,38 @@ const validate = (values: EventApplication) => {
 
   if (!values.address) {
     errors.address = 'Bitte eine Adresse angeben';
+  }
+
+  if (!values.date) {
+    errors.date = 'Bitte ein Datum angeben';
+  }
+
+  if (!values.setup_start) {
+    errors.setup_start =
+      'Bitte eine Uhrzeit für den Beginn der Aufbauarbeiten angeben';
+  }
+
+  const earliestStart = setHours(values.event_start, 6);
+  if (!values.event_start) {
+    errors.event_start =
+      'Bitte eine Uhrzeit für den Beginn der Veranstaltung angeben';
+  } else if (isBefore(values.event_start, earliestStart)) {
+    errors.event_start =
+      'Bitte wählen Sie hier einen Beginn für die Veranstaltung ab frühestens 6 Uhr';
+  }
+
+  const latestEnd = setHours(values.event_end, 22);
+  if (!values.event_end) {
+    errors.event_end =
+      'Bitte eine Uhrzeit für das Ende der Veranstaltung angeben';
+  } else if (isAfter(values.event_end, latestEnd)) {
+    errors.event_end =
+      'Bitte wählen Sie hier ein Ende für die Veranstaltung bis spätestens 22 Uhr';
+  }
+
+  if (!values.teardown_end) {
+    errors.teardown_end =
+      'Bitte eine Uhrzeit für das Ende der Abbauarbeiten angeben';
   }
 
   logger('Validation', errors, values);
