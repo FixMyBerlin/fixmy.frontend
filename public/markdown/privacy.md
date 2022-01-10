e8da4a1bb5a4e42a89dfd834ab9f26bb4d0ae361
Henri <henri@fixmycity.de>
2022-01-10 12:37:04 +0100

    Add React Helmet & noindex meta tag to privacy/imprint markdown page

--- public/markdown/privacy.md
+++ public/markdown/privacy.md
@@ -1,3 +1,5 @@
+<!-- This page contains a noindex meta tag, see src/pages/Markdown/index.js  -->
+
 # Datenschutzerklärung
 
 Mit der folgenden Datenschutzerklärung möchten wir Sie darüber aufklären, welche Arten Ihrer personenbezogenen Daten (nachfolgend auch kurz als "Daten“ bezeichnet) wir zu welchen Zwecken und in welchem Umfang verarbeiten. Die Datenschutzerklärung gilt für alle von uns durchgeführten Verarbeitungen personenbezogener Daten, sowohl im Rahmen der Erbringung unserer Leistungen als auch insbesondere auf unseren Webseiten, in mobilen Applikationen sowie innerhalb externer Onlinepräsenzen, wie z.B. unserer Social-Media-Profile (nachfolgend zusammenfassend bezeichnet als "Onlineangebot“).
--- src/pages/Markdown/index.js
+++ src/pages/Markdown/index.js
@@ -1,10 +1,22 @@
 import ky from 'ky';
 import React, { PureComponent } from 'react';
+import { Helmet } from 'react-helmet';
 
 import ContentPageWrapper from '~/components/ContentPageWrapper';
 import MarkdownContent from '~/pages/Markdown/components/MarkdownContent';
 
 class MarkdownPage extends PureComponent {
+  static addNoindexMeta() {
+    return (
+      <Helmet>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+    );
+  }
+
+  // Hardcode page names which should include noindex meta tag
+  noindexPages = ['imprint', 'privacy'];
+
   constructor(props) {
     super(props);
     this.state = {
@@ -29,8 +41,14 @@ class MarkdownPage extends PureComponent {
   }
 
   render() {
+    let helmet;
+    if (this.noindexPages.includes(this.props.page)) {
+      helmet = MarkdownPage.addNoindexMeta();
+    }
+
     return (
       <ContentPageWrapper>
+        {helmet}
         <MarkdownContent content={this.state.content} />
       </ContentPageWrapper>
     );
