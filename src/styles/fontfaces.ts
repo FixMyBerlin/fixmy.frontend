dcdab39a2b5cb499a12863374e65388899144347
Tobias <tobias@fixmycity.de>
2022-02-01 17:17:57 +0100

    Fonts: Update OpenSans to v27
    - Update to newest version; unclear what that does but I did not want separate versions when adding new variants.
    - Add regular-italic. This is potentially a breaking change since it fell back to a bolder version before without adding any styles. We will need to fix those issues when they come up.
    - I added the files for all variants but did only include what we need right now; we can more easily update and use them this way.

--- src/styles/fontfaces.ts
+++ src/styles/fontfaces.ts
@@ -1,15 +1,16 @@
-import theMixBlackwoff from '../fonts/TheMixC5-9_Black.woff';
-import theMixBlackwoff2 from '../fonts/TheMixC5-9_Black.woff2';
-import openSans300woff from '../fonts/open-sans-v17-latin-300.woff';
-import openSans300woff2 from '../fonts/open-sans-v17-latin-300.woff2';
-import openSans600woff from '../fonts/open-sans-v17-latin-600.woff';
-import openSans600woff2 from '../fonts/open-sans-v17-latin-600.woff2';
-import openSans600woffItalic from '../fonts/open-sans-v17-latin-600italic.woff';
-import openSans600woff2Italic from '../fonts/open-sans-v17-latin-600italic.woff2';
-import openSans700woff from '../fonts/open-sans-v17-latin-700.woff';
-import openSans700woff2 from '../fonts/open-sans-v17-latin-700.woff2';
-import openSansRegularwoff from '../fonts/open-sans-v17-latin-regular.woff';
-import openSansRegularwoff2 from '../fonts/open-sans-v17-latin-regular.woff2';
+// Download from https://google-webfonts-helper.herokuapp.com/fonts/open-sans?subsets=latin
+import openSans300woff from '../fonts/open-sans-v27-latin-300.woff';
+import openSans300woff2 from '../fonts/open-sans-v27-latin-300.woff2';
+import openSans600woff from '../fonts/open-sans-v27-latin-600.woff';
+import openSans600woff2 from '../fonts/open-sans-v27-latin-600.woff2';
+import openSans600woffItalic from '../fonts/open-sans-v27-latin-600italic.woff';
+import openSans600woff2Italic from '../fonts/open-sans-v27-latin-600italic.woff2';
+import openSans700woff from '../fonts/open-sans-v27-latin-700.woff';
+import openSans700woff2 from '../fonts/open-sans-v27-latin-700.woff2';
+import openSans400woffItalic from '../fonts/open-sans-v27-latin-italic.woff';
+import openSans400woff2Italic from '../fonts/open-sans-v27-latin-italic.woff2';
+import openSans400woff from '../fonts/open-sans-v27-latin-regular.woff';
+import openSans400woff2 from '../fonts/open-sans-v27-latin-regular.woff2';
 import robotoSlab100woff from '../fonts/roboto-slab-v12-latin-100.woff';
 import robotoSlab100woff2 from '../fonts/roboto-slab-v12-latin-100.woff2';
 import robotoSlab700woff from '../fonts/roboto-slab-v12-latin-700.woff';
@@ -22,9 +23,10 @@ import robotoItalicwoff from '../fonts/roboto-v20-latin-italic.woff';
 import robotoItalicwoff2 from '../fonts/roboto-v20-latin-italic.woff2';
 import robotowoff from '../fonts/roboto-v20-latin-regular.woff';
 import robotowoff2 from '../fonts/roboto-v20-latin-regular.woff2';
+import theMixBlackwoff from '../fonts/TheMixC5-9_Black.woff';
+import theMixBlackwoff2 from '../fonts/TheMixC5-9_Black.woff2';
 
 export default `
-  /* open-sans-300 - latin */
   @font-face {
     font-family: 'Open Sans';
     font-style: normal;
@@ -41,10 +43,9 @@ export default `
     font-weight: 400;
     font-display: swap;
     src: local('Open Sans Regular'), local('OpenSans-Regular'),
-        url(${openSansRegularwoff2}) format('woff2'),
-        url(${openSansRegularwoff}) format('woff');
+        url(${openSans400woff2}) format('woff2'),
+        url(${openSans400woff}) format('woff');
   }
-  /* open-sans-600 - latin */
   @font-face {
     font-family: 'Open Sans';
     font-style: normal;
@@ -53,6 +54,24 @@ export default `
     src: local('Open Sans SemiBold'), local('OpenSans-SemiBold'),
         url(${openSans600woff2}) format('woff2'),
         url(${openSans600woff}) format('woff');
+  }
+    @font-face {
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local('Open Sans Bold'), local('OpenSans-Bold'),
+        url(${openSans700woff2}) format('woff2'),
+        url(${openSans700woff}) format('woff');
+  }
+  @font-face {
+    font-family: 'Open Sans';
+    font-style: italic;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Open Sans SemiBold Italic'), local('OpenSans-SemiBoldItalic'),
+        url(${openSans400woff2Italic}) format('woff2'),
+        url(${openSans400woffItalic}) format('woff');
   }
   @font-face {
     font-family: 'Open Sans';
@@ -63,7 +82,6 @@ export default `
         url(${openSans600woff2Italic}) format('woff2'),
         url(${openSans600woffItalic}) format('woff');
   }
-  /* open-sans-700 - latin */
   @font-face {
     font-family: 'Open Sans';
     font-style: normal;
@@ -136,7 +154,7 @@ export default `
         url(${roboto700woff2}) format('woff2'),
         url(${roboto700woff}) format('woff');
   }
-  
+
   /* The Mix - black */
   @font-face {
     font-family: 'The Mix';
