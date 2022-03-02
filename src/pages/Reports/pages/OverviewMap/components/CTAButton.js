34fe0e2636b0c0cd181b5a3820a65b27ae98d2be
Laurenz <laurenz.rasche@gmail.com>
2022-03-02 15:25:54 +0100

    move .svg files to components directories

--- src/pages/Reports/pages/OverviewMap/components/CTAButton.js
+++ src/pages/Reports/pages/OverviewMap/components/CTAButton.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Plus from '~/images/plus.svg';
+import Plus from './plus.svg';
 import styled from 'styled-components';
 
 import config from '~/pages/Reports/config';
