26c70b1cd551d9fe21a61ed7e740b5fd47f97f6c
Henri <henri@fixmycity.de>
2022-03-23 13:52:35 +0100

    Remove tempelberg anywhere in the code

--- src/apps/Gastro/components/Notice.tsx
+++ src/apps/Gastro/components/Notice.tsx
@@ -24,32 +24,6 @@ const GastroNotice = ({ district }) => {
       );
   }
 
-  if (district?.name === 'tempelberg') {
-    if (preSignup(district))
-      return (
-        <Notice>
-          Die Anmeldung ist ab{' '}
-          {district.apps.gastro.timeline.openSignup
-            .date()
-            .toLocaleString('de-DE')}{' '}
-          möglich.
-        </Notice>
-      );
-    if (openSignup(district))
-      return (
-        <Notice>
-          Bitte füllen Sie das Formular bis zum Dienstag, den 23. Juni 2020 um
-          10 Uhr aus.
-        </Notice>
-      );
-    if (postSignup(district))
-      return (
-        <Notice>
-          Die eingereichten Meldungen werden vom Bezirksamt geprüft. Eine
-          Bedarfsmeldung ist derzeit nicht möglich (Frist endete zum 22. Juni).
-        </Notice>
-      );
-  }
   return null;
 };
 
