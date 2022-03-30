9231203ff9ab73004ea8e1e3789c48e6abd2ba1d
Laurenz <laurenz.rasche@gmail.com>
2022-03-30 14:37:04 +0200

    use `DSN` from environment variables

--- src/index.js
+++ src/index.js
@@ -8,13 +8,8 @@ import App from '~/App';
 import Store from '~/store';
 
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 
 Sentry.init({
-  dsn: 'https://024d002dd1ca469a8b939e6dcec0af40@o1174824.ingest.sentry.io/6294496',
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 0.0,
 });
 
